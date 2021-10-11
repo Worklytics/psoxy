f93f43b2a26a7f734588a2cb3fefa9aa87786011
Erik Schultink <erik@worklytics.co>
2021-10-11 16:21:46 -0700

    users case

--- java/src/test/java/co/worklytics/psoxy/rules/google/DirectoryTests.java
+++ java/src/test/java/co/worklytics/psoxy/rules/google/DirectoryTests.java
@@ -39,4 +39,20 @@ public class DirectoryTests {
         assertFalse(sanitized.contains("alice@worklytics.co"));
         assertFalse(sanitized.contains("alice.example@gmail.com"));
     }
+
+    @Test
+    void users() {
+        String jsonString = new String(TestUtils.getData("api-response-examples/g-workspace/directory/users.json"));
+
+        //verify precondition that example actually contains something we need to pseudonymize
+        assertTrue(jsonString.contains("alice@worklytics.co"));
+        assertTrue(jsonString.contains("bob@worklytics.co"));
+
+        String sanitized =
+            sanitizer.sanitize(new GenericUrl("https://admin.googleapis.com/admin/directory/v1/users?customer=my_customer"), jsonString);
+
+        assertFalse(sanitized.contains("alice@worklytics.co"));
+        assertFalse(sanitized.contains("alice.example@gmail.com"));
+        assertFalse(sanitized.contains("bob@worklytics.co"));
+    }
 }
