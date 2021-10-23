dafb98a1ef227e3fe29208da21a5283b69c9a58c
Erik Schultink <erik@worklytics.co>
2021-10-22 21:39:36 -0700

    initial working example of util to pseudonymize csv

---
+++ java/impl/cmd-line/README.md
@@ -0,0 +1,19 @@
+# cmd-line
+
+Use this tool to pseudonymize CSV files consistently with Psoxy instances running in the cloud
+(assuming all configured with the same salt).
+
+First, ensure you have java, Maven, etc installed.
+
+Second, create a `config.yaml` file within `java/impl/cmd-line` in your checkout, with your salt and
+a list of which columns in your CSV need to be pseudonymized. See `example-config.yaml` for an
+example. The salt value MUST match that configured for any psoxy instances with which you want
+your pseudonyms to be consistent (eg, so pseudonymixed identifier produced from CSV will match
+the pseudonym produced by the psoxy instance for the same identifier).
+
+Usage:  (from `java/impl/cmd-line`)
+```shell
+mvn clean package
+
+java -jar target/psoxy-cmd-line-1.0-SNAPSHOT.jar src/test/resources/hris-example.csv
+```
--- java/impl/cmd-line/src/main/java/co/worklytics/psoxy/Main.java
+++ java/impl/cmd-line/src/main/java/co/worklytics/psoxy/Main.java
@@ -1,8 +1,104 @@
 package co.worklytics.psoxy;
 
+import co.worklytics.psoxy.impl.SanitizerImpl;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+import com.google.common.base.Preconditions;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+
+import java.io.File;
+import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    private static final String DEFAULT_CONFIG_FILE = "config.yaml";
+
+    static ObjectMapper yamlMapper = new ObjectMapper(new YAMLFactory());
+    static ObjectMapper jsonMapper = new ObjectMapper();
+
+    @lombok.SneakyThrows
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        File configFile = new File(DEFAULT_CONFIG_FILE);
+        Config config;
+        if (configFile.exists()) {
+            config = yamlMapper.readerFor(Config.class).readValue(configFile);
+        } else {
+            throw new Error("No config.yaml found");
+        }
+
+        Sanitizer sanitizer = new SanitizerImpl(Sanitizer.Options.builder()
+            .defaultScopeId(config.getDefaultScopeId())
+            .pseudonymizationSalt(config.getPseudonymizationSalt())
+            .build());
+
+        File inputFile = new File(args[0]);
+        Preconditions.checkArgument(inputFile.exists(), "File %s does not exist", args[0]);
+
+        try (FileReader in = new FileReader(inputFile)) {
+            CSVParser records = CSVFormat.DEFAULT
+                .withFirstRecordAsHeader()
+                .parse(in);
+
+
+            String[] header = records.getHeaderMap().entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toList())
+                .toArray(new String[records.getHeaderMap().size()]);
+
+            config.getColumnsToPseudonymize().stream()
+                    .forEach(columnToPseudonymize ->
+                        Preconditions.checkArgument(records.getHeaderMap().containsKey(columnToPseudonymize), "Column %s to be pseudonymized not in file", columnToPseudonymize));
+
+            CSVPrinter printer = new CSVPrinter(System.out, CSVFormat.DEFAULT.withHeader(header));
+
+            records.forEach(row -> {
+
+                List<Object> sanitized = records.getHeaderMap().entrySet()
+                    .stream()
+                    .sorted(Map.Entry.comparingByValue())
+                    .map(column -> {
+                        if (config.getColumnsToPseudonymize().contains(column.getKey())) {
+                            try {
+                                //q: need extra escaping??
+                                return jsonMapper.writeValueAsString(sanitizer.pseudonymize(row.get(column.getValue())));
+                            } catch (JsonProcessingException e) {
+                                throw new RuntimeException(e);
+                            }
+                        } else {
+                            return row.get(column.getValue());
+                        }
+                    })
+                    .collect(Collectors.toList());
+
+
+                try {
+                    printer.printRecord(sanitized);
+                } catch (Throwable e) {
+                    throw new RuntimeException("Failed to write row", e);
+                }
+            });
+        }
+    }
+
+    @Getter
+    @NoArgsConstructor //for Jackson
+    public static class Config {
+
+        String defaultScopeId;
+
+        Set<String> columnsToPseudonymize;
+
+        String pseudonymizationSalt;
     }
 }
