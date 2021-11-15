23c4220850b5af2a40138231204cefe95bd0c900
Jose Lorenzo <jose@worklytics.co>
2021-11-15 12:14:55 -0800

    Fix and add tests around allowed endpoints

--- java/core/src/main/java/co/worklytics/psoxy/rules/slack/PrebuiltSanitizerRules.java
+++ java/core/src/main/java/co/worklytics/psoxy/rules/slack/PrebuiltSanitizerRules.java
@@ -13,18 +13,18 @@ import static co.worklytics.psoxy.Rules.Rule;
 public class PrebuiltSanitizerRules {
 
     static final Rules SLACK = Rules.builder()
-        .allowedEndpointRegex("^/api/discovery.conversations.list.*")
-        .allowedEndpointRegex("^/api/discovery.conversations.history.*")
-        .allowedEndpointRegex("^/api/discovery.users.list.*")
+        .allowedEndpointRegex("^\\/api\\/discovery\\.conversations\\.list(?:\\?.+)?")
+        .allowedEndpointRegex("^\\/api\\/discovery\\.conversations\\.history(?:\\?.+)?")
+        .allowedEndpointRegex("^\\/api\\/discovery\\.users\\.list(?:\\?.+)?")
         // users
         .pseudonymization(Rule.builder()
-            .relativeUrlRegex("/api/discovery.users.list.*")
+            .relativeUrlRegex("\\/api\\/discovery\\.users\\.list(?:\\?.+)?")
             .jsonPath("$.users[*].id")
             .jsonPath("$.users[*].profile.email")
             .build()
         )
         .redaction(Rule.builder()
-            .relativeUrlRegex("/api/discovery.users.list.*")
+            .relativeUrlRegex("\\/api\\/discovery\\.users\\.list(?:\\?.+)?")
             // we don't care about names
             .jsonPath("$.users[*]['name','real_name']")
             .jsonPath("$.users[*].profile['name','real_name','display_name','display_name_normalized','real_name_normalized','title','phone','skype','first_name','last_name']")
@@ -34,7 +34,7 @@ public class PrebuiltSanitizerRules {
         // no PII
         // redact channel name, topic and purpose
         .redaction(Rule.builder()
-            .relativeUrlRegex("/api/discovery.conversations.list.*")
+            .relativeUrlRegex("\\/api\\/discovery\\.conversations\\.list(?:\\?.+)?")
             // we don't care about names
             // topic and purpose contains user ids, not used at all, so just get rid of the entire content
             .jsonPath("$.channels[*]['name','topic','purpose']")
@@ -44,14 +44,14 @@ public class PrebuiltSanitizerRules {
         // no PII
         // redact channel name, topic and purpose
         .pseudonymization(Rule.builder()
-            .relativeUrlRegex("/api/discovery.conversations.history.*")
+            .relativeUrlRegex("\\/api\\/discovery\\.conversations\\.history(?:\\?.+)?")
             .jsonPath("$.messages[*].user")
             .jsonPath("$.messages[*].reactions[*].users[*]")
             .jsonPath("$.messages[*].edited.user")
             .build()
         )
         .redaction(Rule.builder()
-            .relativeUrlRegex("/api/discovery.conversations.history.*")
+            .relativeUrlRegex("\\/api\\/discovery\\.conversations\\.history(?:\\?.+)?")
             // we don't care about text
             // username is a variation of user, so just skip it to avoid references
             .jsonPath("$.messages[*]['text','username']")
