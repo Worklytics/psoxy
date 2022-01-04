06233bea2e8e9906b763f1099b6c261a713ab65d
Erik Schultink <erik@worklytics.co>
2022-01-04 14:26:40 -0800

    module to provision GCP sa auth key and save as secret in AWS

---
+++ infra/modules/gcp-sa-auth-key-aws-secret/variables.tf
@@ -0,0 +1,21 @@
+
+variable "service_account_id" {
+  type        = string
+  description = "ID of the service account"
+}
+
+variable "secret_project" {
+  type        = string
+  description = "ID of project in which to store SA key as secret"
+}
+
+variable "secret_id" {
+  type        = string
+  description = "ID to give secret for SA key"
+}
+
+variable "rotation_days" {
+  type        = number
+  default     =  60
+  description = "rotation period for the SA key, in days"
+}
