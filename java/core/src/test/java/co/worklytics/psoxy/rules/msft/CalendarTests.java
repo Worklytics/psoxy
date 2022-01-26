2cde2bb2a8a8284f7d0f55368583890e86d3db6d
Erik Schultink <erik@worklytics.co>
2022-01-26 11:33:49 -0800

    msft calendar rules + tests

--- java/core/src/test/java/co/worklytics/psoxy/rules/msft/CalendarTests.java
+++ java/core/src/test/java/co/worklytics/psoxy/rules/msft/CalendarTests.java
@@ -3,6 +3,8 @@ package co.worklytics.psoxy.rules.msft;
 import co.worklytics.psoxy.Rules;
 import co.worklytics.psoxy.rules.JavaRulesTestBaseCase;
 import lombok.Getter;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CalendarTests extends JavaRulesTestBaseCase {
 
@@ -17,4 +19,91 @@ public class CalendarTests extends JavaRulesTestBaseCase {
 
     @Getter
     final String yamlSerializationFilepath = "microsoft-365/outlook-cal";
+
+
+    @ParameterizedTest
+    @ValueSource(strings = {"v1.0", "beta"})
+    void events(String apiVersion) {
+        String endpoint = "https://graph.microsoft.com/" + apiVersion +
+            "/users/48d31887-5fad-4d73-a9f5-3c356e68a038/events";
+
+        String jsonResponse = asJson("Events_" + apiVersion + ".json");
+
+        String sanitized = sanitize(endpoint, jsonResponse);
+
+        assertRedacted(sanitized,
+            "Irvin Sayers",
+            "New Product Regulations Touchpoint", //subject
+            "New Product Regulations Strategy Online Touchpoint Meeting" //body
+        );
+
+        assertPseudonymized(sanitized,
+            "engineering@M365x214355.onmicrosoft.com",
+            "IrvinS@M365x214355.onmicrosoft.com"
+        );
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"v1.0", "beta"})
+    void calendarEvents(String apiVersion) {
+        String endpoint = "https://graph.microsoft.com/" + apiVersion +
+            "/users/48d31887-5fad-4d73-a9f5-3c356e68a038/calendars/AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEGAAAiIsqMbYjsT5e-T7KzowPTAAABuC35AAA=/events";
+
+        String jsonResponse = asJson("CalendarEvents_" + apiVersion + ".json");
+
+        String sanitized = sanitize(endpoint, jsonResponse);
+
+        assertRedacted(sanitized,
+            "Irvin Sayers",
+            "New Product Regulations Touchpoint", //subject
+            "New Product Regulations Strategy Online Touchpoint Meeting" //body
+        );
+
+        assertPseudonymized(sanitized,
+            "engineering@M365x214355.onmicrosoft.com",
+            "IrvinS@M365x214355.onmicrosoft.com"
+        );
+
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"v1.0", "beta"})
+    void event(String apiVersion) {
+        String endpoint = "https://graph.microsoft.com/" + apiVersion +
+            "/users/48d31887-5fad-4d73-a9f5-3c356e68a038/events";
+
+        String jsonResponse = asJson("Event_" + apiVersion + ".json");
+
+        assertNotSanitized(jsonResponse,
+            "engineering@M365x214355.onmicrosoft.com",
+            "IrvinS@M365x214355.onmicrosoft.com",
+            "Irvin Sayers",
+            "New Product Regulations Touchpoint", //subject
+            "New Product Regulations Strategy Online Touchpoint Meeting" //body
+        );
+
+        String sanitized = sanitize(endpoint, jsonResponse);
+
+        assertRedacted(sanitized,
+            "Irvin Sayers",
+            "New Product Regulations Touchpoint", //subject
+            "New Product Regulations Strategy Online Touchpoint Meeting" //body
+            );
+
+        assertPseudonymized(sanitized,
+            "engineering@M365x214355.onmicrosoft.com",
+            "IrvinS@M365x214355.onmicrosoft.com"
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"v1.0", "beta"})
+    public void mailboxSettings(String apiVersion) {
+        String endpoint = "https://graph.microsoft.com/" + apiVersion +
+            "/users/48d31887-5fad-4d73-a9f5-3c356e68a038/mailboxSettings";
+
+        assertUrlAllowed(endpoint);
+        assertUrlWithQueryParamsBlocked(endpoint);
+    }
 }
