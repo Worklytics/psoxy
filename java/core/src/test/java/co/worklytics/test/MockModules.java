3d6fe22d5357b12ae3da740980e1b776c89b8e71
Jose Lorenzo <jose@worklytics.co>
2022-02-17 15:56:15 -0800

    Test to show error

--- java/core/src/test/java/co/worklytics/test/MockModules.java
+++ java/core/src/test/java/co/worklytics/test/MockModules.java
@@ -1,8 +1,11 @@
 package co.worklytics.test;
 
+import co.worklytics.psoxy.Rules;
 import co.worklytics.psoxy.gateway.ConfigService;
+import co.worklytics.psoxy.gateway.SourceAuthStrategy;
 import dagger.Module;
 import dagger.Provides;
+import dagger.multibindings.IntoSet;
 
 import javax.inject.Singleton;
 
@@ -12,10 +15,25 @@ public class MockModules {
 
     @Module
     public interface ForConfigService {
-
         @Provides @Singleton
         static ConfigService configService() {
             return mock(ConfigService.class);
+        }
+    }
+
+    @Module
+    public interface ForRules {
+        @Provides @Singleton
+        static Rules rules() {
+            return mock(Rules.class);
+        }
+    }
+
+    @Module
+    public interface ForSourceAuthStrategySet {
+        @Provides @IntoSet
+        static SourceAuthStrategy sourceStratey() {
+            return mock(SourceAuthStrategy.class);
         }
     }
 
