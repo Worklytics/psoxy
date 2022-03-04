89822c344d73b4db016041f4d7444aa8faaa4efa
Erik Schultink <erik@worklytics.co>
2022-03-04 15:35:20 -0800

    add Worklytics instructions in aws-msft examples

--- infra/examples/aws-msft-365/main.tf
+++ infra/examples/aws-msft-365/main.tf
@@ -194,3 +194,13 @@ module "msft_365_grants" {
   app_roles                = each.value.required_app_roles
   application_name         = each.key
 }
+
+
+module "worklytics-psoxy-connection" {
+  for_each = local.msft_sources
+
+  source = "../../modules/worklytics-psoxy-connection"
+
+  psoxy_endpoint_url = module.psoxy-msft-connector[each.key].endpoint_url
+  display_name       = "${each.value.display_name} via Psoxy"
+}
