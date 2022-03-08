3eb4412b2fc422e7e9cc8d2bab56f2f25969a9d8
Andrés Pérez <andres@worklytics.co>
2022-03-08 18:17:49 +0100

    Removed BOM flag from enum

--- java/impl/aws/src/main/java/co/worklytics/psoxy/AWSConfigProperty.java
+++ java/impl/aws/src/main/java/co/worklytics/psoxy/AWSConfigProperty.java
@@ -3,7 +3,6 @@ package co.worklytics.psoxy;
 import co.worklytics.psoxy.gateway.ConfigService;
 
 public enum AWSConfigProperty implements ConfigService.ConfigProperty {
-    BOM_ENCODED, // mark if file is encoded with Byte Order Mark (BOM)
     IMPORT_BUCKET,
     OUTPUT_BUCKET;
 }
