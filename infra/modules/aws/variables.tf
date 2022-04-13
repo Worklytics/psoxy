e950506e035203d316cea1443f6e334883365f10
Erik Schultink <erik@worklytics.co>
2022-04-13 13:43:12 -0700

    style in variables

--- infra/modules/aws/variables.tf
+++ infra/modules/aws/variables.tf
@@ -8,9 +8,9 @@ variable "aws_account_id" {
 }
 
 variable "aws_account_with_gcp_auth" {
-  default     = "939846301470:root" # Worklytics's account to which its GCP identities are federated
   type        = string
   description = "id of AWS account that will authenticate GCP SAs (eg has GCP OIDC configured); any user added to 'allowed_gcp_callers' must be auth'd by this AWS account"
+  default     = "939846301470:root" # Worklytics's account to which its GCP identities are federated
   validation {
     condition     = can(regex("^\\d{12}:\\w+$", var.aws_account_with_gcp_auth))
     error_message = "The aws_account_id value should be 12-digit numeric string, followed by ':root'."
