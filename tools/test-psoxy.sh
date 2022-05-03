b0a286fec171f3d8b8f0ad106974e911073102a1
Jose Lorenzo <jose@worklytics.co>
2022-05-02 17:03:24 -0700

    Add impersonation flag to test-psoxy.sh

--- tools/test-psoxy.sh
+++ tools/test-psoxy.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 ECHO=false
-SANITIZATION_HEADER="X-Psoxy-Skip-Sanitizer: false"
 
 function show_help() {
+    echo "Basic call: ./test-psoxy.sh [-g|-a -r ROLE] [-i] -u https://url-to-proxy-function"
     echo "-g endpoint is GCP"
     echo "-a endpoint is AWS"
     echo "-r AWS role to impersonate"
+    echo "-i user to impersonate, needed for certain connectors"
     echo "-u URL to call"
     echo "-v verbose"
-    echo "-o omit sanitization rules, only works if function deployed in development mode"
+    echo "-s skip sanitization rules, only works if function deployed in development mode"
     echo "-h show this help"
     exit 0
 }
 
-while getopts gahvor:u: flag
+while getopts gahvsr:u:i: flag
 do
     case "${flag}" in
         g) GCP=true;;
         a) AWS=true;;
         r) ROLE_ARN=${OPTARG};;
-        v) ECHO=true;;
+        v) ECHO=true; curlparams+=("-v");;
         u) TEST_URL=${OPTARG};;
-        o) SANITIZATION_HEADER="X-Psoxy-Skip-Sanitizer: true";;
+        s) curlparams+=("-HX-Psoxy-Skip-Sanitizer: true");;
+        i) curlparams+=("-HX-Psoxy-User-To-Impersonate: ${OPTARG}");;
         h) show_help;;
     esac
 done
@@ -43,17 +45,19 @@ then
   log "Calling proxy..."
   log "Request: $TEST_URL"
   log "Waiting Response:"
-  awscurl -v -H "$SANITIZATION_HEADER" --service execute-api --access_key $CALLER_ACCESS_KEY_ID --secret_key $CALLER_SECRET_ACCESS_KEY --security_token $CALLER_SESSION_TOKEN $TEST_URL
+  awscurl "${curlparams[@]}" --service execute-api --access_key $CALLER_ACCESS_KEY_ID --secret_key $CALLER_SECRET_ACCESS_KEY --security_token $CALLER_SESSION_TOKEN $TEST_URL
   # Remove env variables
   unset CALLER_ACCESS_KEY_ID CALLER_SECRET_ACCESS_KEY CALLER_SESSION_TOKEN
 fi
 
 if [ "$GCP" = true ];
 then
+  SANITIZATION_HEADER="X-Psoxy-Skip-Sanitizer: false"
+
   log "Getting token"
-  CURL_AUTH_HEADER="Authorization: Bearer $(gcloud auth print-identity-token)"
+  curlparams+=("-HAuthorization: Bearer $(gcloud auth print-identity-token)")
   log "Calling proxy..."
   log "Request: $TEST_URL"
   log "Waiting Response:"
-  curl -X GET -H "$CURL_AUTH_HEADER" -H "$SANITIZATION_HEADER" $TEST_URL
+  curl "${curlparams[@]}" $TEST_URL
 fi
