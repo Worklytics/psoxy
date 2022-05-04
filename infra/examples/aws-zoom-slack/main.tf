bfac4597adb9525feb98f765e3b161d5c17485cd
Erik Schultink <erik@worklytics.co>
2022-05-03 17:41:31 -0700

    modernize style

--- infra/examples/aws-zoom-slack/main.tf
+++ infra/examples/aws-zoom-slack/main.tf
@@ -76,7 +76,7 @@ resource "aws_ssm_parameter" "long-access-token-secret" {
   value       = sensitive("TODO: fill me with a real token!! (via AWS console)")
 
   ignore_changes = [
-    "value" # we expect this to be filled via Console, so don't want to overwrite it with the dummy value if changed
+    value # we expect this to be filled via Console, so don't want to overwrite it with the dummy value if changed
   ]
 }
 
