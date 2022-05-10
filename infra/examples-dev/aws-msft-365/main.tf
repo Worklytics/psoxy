72d9f7a06b6a78aa03cc1ca9f25cd8d9c7c5b5b5
Jose Lorenzo <jose@worklytics.co>
2022-05-09 21:23:25 -0700

    Add missing variable

--- infra/examples-dev/aws-msft-365/main.tf
+++ infra/examples-dev/aws-msft-365/main.tf
@@ -175,15 +175,14 @@ module "psoxy-msft-connector" {
 
   source = "../../modules/aws-psoxy-instance"
 
-  function_name            = "psoxy-${each.key}"
-  source_kind              = each.value.source_kind
-  path_to_function_zip     = module.psoxy-package.path_to_deployment_jar
-  function_zip_hash        = module.psoxy-package.deployment_package_hash
-  path_to_config           = "${var.psoxy_base_dir}/configs/${each.value.source_kind}.yaml"
-  api_caller_role_arn      = module.psoxy-aws.api_caller_role_arn
-  api_caller_role_arn_name = module.psoxy-aws.api_caller_role_name
-  aws_assume_role_arn      = var.aws_assume_role_arn
-  example_api_calls        = each.value.example_calls
+  function_name        = "psoxy-${each.key}"
+  source_kind          = each.value.source_kind
+  path_to_function_zip = module.psoxy-package.path_to_deployment_jar
+  function_zip_hash    = module.psoxy-package.deployment_package_hash
+  path_to_config       = "${var.psoxy_base_dir}/configs/${each.value.source_kind}.yaml"
+  aws_assume_role_arn  = var.aws_assume_role_arn
+  example_api_calls    = each.value.example_calls
+  aws_account_id       = var.aws_account_id
 
   parameters = concat(
     module.private-key-aws-parameters[each.key].parameters,
