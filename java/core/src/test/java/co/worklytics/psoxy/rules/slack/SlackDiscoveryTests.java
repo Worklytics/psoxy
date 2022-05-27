1aadc2e41e3ce8423b593a4234a4755a03364042
Andrés Pérez <andres@worklytics.co>
2022-05-27 12:55:43 +0200

    Added tests

--- java/core/src/test/java/co/worklytics/psoxy/rules/slack/SlackDiscoveryTests.java
+++ java/core/src/test/java/co/worklytics/psoxy/rules/slack/SlackDiscoveryTests.java
@@ -34,15 +34,15 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
 
     @SneakyThrows
     @ValueSource(strings = {
-        "https://slack.com/api/discovery.enterprise.info",
-        "https://slack.com/api/discovery.conversations.list#fragment", // fragments get discarded
-        "https://slack.com/api/discovery.conversations.list",
-        "https://slack.com/api/discovery.conversations.list?team=X&offset=Y&only_im=true",
-        "https://slack.com/api/discovery.conversations.history",
-        "https://slack.com/api/discovery.conversations.history?channel=X&limit=10",
-        "https://slack.com/api/discovery.conversations.recent?team=X&limit=10&latest=123",
-        "https://slack.com/api/discovery.users.list",
-        "https://slack.com/api/discovery.users.list?limit=20&include_deleted=true",
+            "https://slack.com/api/discovery.enterprise.info",
+            "https://slack.com/api/discovery.conversations.list#fragment", // fragments get discarded
+            "https://slack.com/api/discovery.conversations.list",
+            "https://slack.com/api/discovery.conversations.list?team=X&offset=Y&only_im=true",
+            "https://slack.com/api/discovery.conversations.history",
+            "https://slack.com/api/discovery.conversations.history?channel=X&limit=10",
+            "https://slack.com/api/discovery.conversations.recent?team=X&limit=10&latest=123",
+            "https://slack.com/api/discovery.users.list",
+            "https://slack.com/api/discovery.users.list?limit=20&include_deleted=true",
     })
     @ParameterizedTest
     void allowedEndpointRegex_allowed(String url) {
@@ -51,33 +51,33 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
 
     @SneakyThrows
     @ValueSource(strings = {
-        // variations on allowed
-        "https://slack.com/api/discovery.conversations.list/",
-        "https://slack.com/api/discovery_conversations-list",
-        "https://slack.com/api/discovery-conversations-history",
-        "https://slack.com/api/discovery users list",
-        "https://slack.com/api/discovery.conversation.info/",
-        // all the rest of the discovery methods
-        "https://slack.com/api/discovery.user.info",
-        "https://slack.com/api/discovery.user.conversations",
-        "https://slack.com/api/discovery.conversations.edits",
-        "https://slack.com/api/discovery.conversations.members",
-        "https://slack.com/api/discovery.conversations.renames",
-        "https://slack.com/api/discovery.conversations.reactions",
-        "https://slack.com/api/discovery.conversations.search",
-        "https://slack.com/api/discovery.chat.info",
-        "https://slack.com/api/discovery.chat.update",
-        "https://slack.com/api/discovery.chat.delete",
-        "https://slack.com/api/discovery.chat.tombstone",
-        "https://slack.com/api/discovery.chat.restore",
-        "https://slack.com/api/discovery.drafts.list",
-        "https://slack.com/api/discovery.draft.info",
-        "https://slack.com/api/discovery.files.list",
-        "https://slack.com/api/discovery.file.info",
-        "https://slack.com/api/discovery.file.tombstone",
-        "https://slack.com/api/discovery.file.restore",
-        "https://slack.com/api/discovery.file.delete",
-        "https://slack.com/api/discovery.files.release",
+            // variations on allowed
+            "https://slack.com/api/discovery.conversations.list/",
+            "https://slack.com/api/discovery_conversations-list",
+            "https://slack.com/api/discovery-conversations-history",
+            "https://slack.com/api/discovery users list",
+            "https://slack.com/api/discovery.conversation.info/",
+            // all the rest of the discovery methods
+            "https://slack.com/api/discovery.user.info",
+            "https://slack.com/api/discovery.user.conversations",
+            "https://slack.com/api/discovery.conversations.edits",
+            "https://slack.com/api/discovery.conversations.members",
+            "https://slack.com/api/discovery.conversations.renames",
+            "https://slack.com/api/discovery.conversations.reactions",
+            "https://slack.com/api/discovery.conversations.search",
+            "https://slack.com/api/discovery.chat.info",
+            "https://slack.com/api/discovery.chat.update",
+            "https://slack.com/api/discovery.chat.delete",
+            "https://slack.com/api/discovery.chat.tombstone",
+            "https://slack.com/api/discovery.chat.restore",
+            "https://slack.com/api/discovery.drafts.list",
+            "https://slack.com/api/discovery.draft.info",
+            "https://slack.com/api/discovery.files.list",
+            "https://slack.com/api/discovery.file.info",
+            "https://slack.com/api/discovery.file.tombstone",
+            "https://slack.com/api/discovery.file.restore",
+            "https://slack.com/api/discovery.file.delete",
+            "https://slack.com/api/discovery.files.release",
     })
     @ParameterizedTest
     void allowedEndpointRegex_blocked(String url) {
@@ -91,14 +91,14 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
 
         //verify precondition that example actually contains something we need to pseudonymize
         Collection<String> PII = Arrays.asList(
-            "john@domain.com",
-            "felipe@domain.com",
-            "bob@domain.com"
+                "john@domain.com",
+                "felipe@domain.com",
+                "bob@domain.com"
         );
         assertNotSanitized(jsonString, PII);
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.users.list"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.users.list"), jsonString);
 
         assertPseudonymized(sanitized, PII);
         assertRedacted(sanitized, "John Nobody", "felipe", "bob");
@@ -116,7 +116,7 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
         String jsonString = asJson("discovery-conversations-list.json");
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.list"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.list"), jsonString);
 
         // nothing to pseudonymize
         assertRedacted(sanitized, "This is the topic", "mpdm-primary-owner--first.person--second.person");
@@ -128,28 +128,30 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
         String jsonString = asJson("discovery-conversations-history.json");
 
         Collection<String> PIItoPseudonymize = Arrays.asList(
-            "W06CA4EAC","W0G81RDQT","W0N0ZQDED","W0R8EBMXP","W0G81RDQZ","W000000", "U02DU306H0B",
-            "REPLYUSER"
+                "W06CA4EAC", "W0G81RDQT", "W0N0ZQDED", "W0R8EBMXP", "W0G81RDQZ", "W000000", "U02DU306H0B",
+                "REPLYUSER",
+                "some parent user"
         );
         Collection<String> dataToRedact = Arrays.asList(
-            "Test message!",
-            "<@U06CA4EAC|bjin>",
-            "text with rich block",
-            "Some new text",
-            "check this out!",
-            "Jose (ENT)",
-            "Jose",
-            "This is likely a pun about the weather.",
-            "We're withholding a pun from you",
-            "Leg end nary a laugh, Ink.",
-            "Some other text"
+                "Test message!",
+                "<@U06CA4EAC|bjin>",
+                "text with rich block",
+                "Some new text",
+                "check this out!",
+                "Jose (ENT)",
+                "Jose",
+                "This is likely a pun about the weather.",
+                "We're withholding a pun from you",
+                "Leg end nary a laugh, Ink.",
+                "Some other text",
+                "https://badpuns.example.com/puns/123.png"
         );
 
         assertNotSanitized(jsonString, PIItoPseudonymize);
         assertNotSanitized(jsonString, dataToRedact);
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.history"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.history"), jsonString);
 
         assertPseudonymized(sanitized, PIItoPseudonymize);
         assertRedacted(sanitized, dataToRedact);
@@ -161,7 +163,7 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
         String jsonString = asJson("discovery-conversations-recent.json");
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.recent"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.recent"), jsonString);
 
         // nothing to redact / pseudonymize
         assertEquals(jsonString, sanitized);
@@ -173,18 +175,18 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
         String jsonString = asJson("discovery-conversations-info.json");
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.info"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.conversations.info"), jsonString);
 
         Collection<String> PII = Arrays.asList(
-            "W0N9HDWUR"
+                "W0N9HDWUR"
         );
 
         assertPseudonymized(sanitized, PII);
         assertRedacted(sanitized, "Collaboration about Project X",
-            "Launch date scheduled for 07/01",
-            "project-x",
-            "project X",
-            "project-y");
+                "Launch date scheduled for 07/01",
+                "project-x",
+                "project X",
+                "project-y");
 
     }
 
@@ -194,14 +196,14 @@ public class SlackDiscoveryTests extends JavaRulesTestBaseCase {
         String jsonString = asJson("discovery-enterprise-info.json");
 
         String sanitized =
-            sanitizer.sanitize(new URL("https://slack.com/api/discovery.enterprise.info"), jsonString);
+                sanitizer.sanitize(new URL("https://slack.com/api/discovery.enterprise.info"), jsonString);
 
         assertRedacted(sanitized, "icon", "image",
-            "DevGrid - W1",
-            "1st Workspace under Test Grid",
-            "DevGrid - W2",
-            "2nd Workspace under Test Grid",
-            "Test Grid");
+                "DevGrid - W1",
+                "1st Workspace under Test Grid",
+                "DevGrid - W2",
+                "2nd Workspace under Test Grid",
+                "Test Grid");
 
     }
 }
