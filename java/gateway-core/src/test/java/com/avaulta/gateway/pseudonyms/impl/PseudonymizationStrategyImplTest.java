357d663bbfbca1ab67a8435412fba6c92d6bd9cc
Erik Schultink <erik@worklytics.co>
2022-07-08 10:56:51 -0700

    test pseudonym behavior

--- java/gateway-core/src/test/java/com/avaulta/gateway/pseudonyms/impl/PseudonymizationStrategyImplTest.java
+++ java/gateway-core/src/test/java/com/avaulta/gateway/pseudonyms/impl/PseudonymizationStrategyImplTest.java
@@ -2,12 +2,16 @@ package com.avaulta.gateway.pseudonyms.impl;
 
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.Random;
 import java.util.function.Function;
+import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 
 class PseudonymizationStrategyImplTest {
@@ -43,5 +47,49 @@ class PseudonymizationStrategyImplTest {
 
         assertEquals("blah",
             pseudonymizationStrategy.getIdentifier("NHXWS5CZDysDs3ETExXiMZxM2DfffirkjgmA64R9hCenHbNbPsOt4W-Hx8SDUaQY"));
+    }
+
+
+    @Test
+    void pseudonymAsKeyPrefix() {
+
+        String keyed = pseudonymizationStrategy.getKeyedPseudonym("blah", Function.identity());
+        String pseudonym = pseudonymizationStrategy.getPseudonym("blah", Function.identity());
+
+
+        byte[] keyedBytes = Base64.getUrlDecoder().decode(keyed);
+        byte[] pseudonymBytes = Base64.getUrlDecoder().decode(pseudonym);
+
+        assertTrue(Arrays.equals(Arrays.copyOfRange(keyedBytes, 0, pseudonymBytes.length), pseudonymBytes),
+            "pseudonym is prefix of keyed");
+    }
+
+
+    @Test
+    void keyedPseudonym_sizes() {
+        Random random = new Random();
+        IntStream.generate(() -> random.nextInt(1000000000)).limit(100).forEach(i -> {
+            String pseudonym = pseudonymizationStrategy.getKeyedPseudonym("blah" + i, Function.identity());
+            assertEquals(
+                PseudonymizationStrategyImpl.PSEUDONYM_SIZE_BYTES * 2, //hash + ciphertext
+                pseudonym.length());
+        });
+    }
+
+
+    @Test
+    void pseudonym_sizes() {
+
+        Random random = new Random();
+        IntStream.generate(() -> random.nextInt(1000000000)).limit(100).forEach(i -> {
+            String pseudonym = pseudonymizationStrategy.getPseudonym("blah" + i, Function.identity());
+
+            //43 bytes when base64-encoded without padding
+            assertEquals(43, pseudonym.length());
+
+            //32 bytes unencoded
+            assertEquals(PseudonymizationStrategyImpl.PSEUDONYM_SIZE_BYTES,
+                Base64.getUrlDecoder().decode(pseudonym).length);
+        });
     }
 }
