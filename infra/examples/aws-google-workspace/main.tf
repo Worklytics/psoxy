d3ecba1380fbc0d343970a8027e7ea3ec1132570
Erik Schultink <erik@worklytics.co>
2022-07-18 12:34:05 -0700

    get rid of zoom/slack as distinct example, to simplify things

--- infra/examples/aws-google-workspace/main.tf
+++ infra/examples/aws-google-workspace/main.tf
@@ -203,3 +203,81 @@ module "worklytics-psoxy-connection-google-workspace" {
   aws_region         = var.aws_region
   aws_role_arn       = module.psoxy-aws.api_caller_role_arn
 }
+
+
+# BEGIN LONG ACCESS AUTH CONNECTORS
+
+locals {
+  oauth_long_access_connectors = {
+    slack-discovery-api = {
+      enabled : true
+      source_kind : "slack"
+      display_name : "Slack Discovery API"
+      example_api_calls : []
+    },
+    zoom = {
+      enabled : true
+      source_kind : "zoom"
+      display_name : "Zoom"
+      example_api_calls : ["/v2/users"]
+    }
+  }
+  enabled_oauth_long_access_connectors = { for k, v in local.oauth_long_access_connectors : k => v if v.enabled }
+  base_config_path                     = "${var.psoxy_base_dir}configs/"
+}
+
+# Create secret (later filled by customer)
+resource "aws_ssm_parameter" "long-access-token-secret" {
+  for_each = local.enabled_oauth_long_access_connectors
+
+  name        = "PSOXY_${upper(replace(each.key, "-", "_"))}_ACCESS_TOKEN"
+  type        = "SecureString"
+  description = "The long lived token for `psoxy-${each.key}`"
+  value       = sensitive("TODO: fill me with a real token!! (via AWS console)")
+
+  lifecycle {
+    ignore_changes = [
+      value # we expect this to be filled via Console, so don't want to overwrite it with the dummy value if changed
+    ]
+  }
+}
+
+module "aws-psoxy-long-auth-connectors" {
+  for_each = local.enabled_oauth_long_access_connectors
+
+  # source = "../../modules/aws-psoxy-rest"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/aws-psoxy-instance?ref=v0.4.0-rc"
+
+
+  function_name        = "psoxy-${each.key}"
+  path_to_function_zip = module.psoxy-aws.path_to_deployment_jar
+  function_zip_hash    = module.psoxy-aws.deployment_package_hash
+  path_to_config       = "${local.base_config_path}/${each.value.source_kind}.yaml"
+  aws_assume_role_arn  = var.aws_assume_role_arn
+  aws_account_id       = var.aws_account_id
+  api_caller_role_arn  = module.psoxy-aws.api_caller_role_arn
+  source_kind          = each.value.source_kind
+  path_to_repo_root    = var.psoxy_base_dir
+
+
+  parameters = [
+    module.psoxy-aws.salt_secret,
+    aws_ssm_parameter.long-access-token-secret[each.key]
+  ]
+  example_api_calls = each.value.example_api_calls
+
+}
+
+module "worklytics-psoxy-connection" {
+  for_each = local.enabled_oauth_long_access_connectors
+
+  # source = "../../modules/worklytics-psoxy-connection-aws"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-psoxy-connection-aws?ref=v0.4.0-rc"
+
+  psoxy_endpoint_url = module.aws-psoxy-long-auth-connectors[each.key].endpoint_url
+  display_name       = "${each.value.display_name} via Psoxy${var.connector_display_name_suffix}"
+  aws_region         = var.aws_region
+  aws_role_arn       = module.psoxy-aws.api_caller_role_arn
+}
+
+# END LONG ACCESS AUTH CONNECTORS
