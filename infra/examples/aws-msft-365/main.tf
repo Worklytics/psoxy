59745af7dc92e59ea5805f9ea341ec115413681b
Erik Schultink <erik@worklytics.co>
2022-08-24 16:25:05 -0700

    slack instructions in examples

--- infra/examples/aws-msft-365/main.tf
+++ infra/examples/aws-msft-365/main.tf
@@ -240,20 +240,78 @@ module "worklytics-psoxy-connection" {
 
 locals {
   oauth_long_access_connectors = {
+    asana = {
+      enabled : false,
+      source_kind : "asana",
+      display_name : "Asana"
+      example_api_calls : [
+        "/api/1.0/teams",
+        "/api/1.0/projects"
+      ]
+      external_token_todo : <<EOT
+  1. Create a [Service Account User + token](https://asana.com/guide/help/premium/service-accounts)
+     or a sufficiently [Personal Access Token]() for a sufficiently privileged user (who can see all
+     the workspaces/teams/projects/tasks you wish to import to Worklytics via this connection).
+EOT
+    }
     slack-discovery-api = {
-      enabled : true
+      enabled : false
       source_kind : "slack"
       display_name : "Slack Discovery API"
       example_api_calls : []
-    },
+      external_token_todo : <<EOT
+## Slack Discovery Setup
+
+For enabling Slack Discovery with the Psoxy you must first setup an app on your Slack Enterprise
+instance.
+  1. Go to https://api.slack.com/apps and create an app, select name a development workspace
+  2. Take note of your App ID and contact your Slack rep and ask them to enable `discovery:read` scope for the app.
+
+If they also enable `discovery:write` then delete it for safety, the app just needs read access.
+
+3. Generate the following URL replacing the placeholders for *YOUR_CLIENT_ID* and *YOUR_APP_SECRET* and save it for later
+
+`https://api.slack.com/api/oauth.v2.access?client_id=YOUR_CLIENT_ID&client_secret=YOUR_APP_SECRET`
+
+4. Go to OAuth & Permissions > Redirect URLs and add the previous URL there
+
+The next step depends on your installation approach you might need to change slightly
+
+### Org wide install
+Use this step if you want to install in the whole org, across multiple workspaces.
+
+  1. Add a bot scope (not really used, but Slack doesn't allow org-wide without a bot scope requested).
+     Just add `users:read`, something that is read-only and we already have access through discovery.
+  2. Go to *Org Level Apps* and Opt-in to the program
+  3. Go to Settings > Install App
+  4. Install into *organization*
+  5. Copy the User OAuth Token
+  6. If you are implementing the Proxy, then add the access token as `PSOXY_ACCESS_TOKEN_psoxy-slack-discovery-api` secret value in the Secret Manager for the Proxy
+  Otherwise, share the token with the AWS/GCP administrator completing the implementation.
+
+### Workspace install
+Use this steps if you intend to install in just one workspace within your org.
+
+  1. Go to Settings > Install App
+  2. Install into *workspace*
+  3. Copy the User OAuth Token and store it in the secret manager (or share with the administrator completing the implementation)
+  4. Add the access token as `PSOXY_ACCESS_TOKEN_psoxy-slack-discovery-api` secret value in the GCP Project's Secret Manager
+EOT
+    }
     zoom = {
       enabled : true
       source_kind : "zoom"
       display_name : "Zoom"
-      example_api_calls : ["/v2/users"]
+      example_api_calls : [
+        "/v2/users"
+      ]
+      external_token_todo : <<EOT
+TODO: Zoom install instructions
+EOT
     }
   }
   enabled_oauth_long_access_connectors = { for k, v in local.oauth_long_access_connectors : k => v if v.enabled }
+  enabled_oauth_long_access_connectors_todos = { for k, v in local.oauth_long_access_connectors : k => v if v.enabled && v.external_token_todo != null }
 }
 
 # Create secret (later filled by customer)
@@ -310,6 +368,18 @@ module "worklytics-psoxy-connection-oauth-long-access" {
   aws_role_arn       = module.psoxy-aws.api_caller_role_arn
 }
 
+
+module "source_token_external_todo" {
+  for_each = local.enabled_oauth_long_access_connectors_todos
+
+  #source = "../../modules/source-token-external-todo"
+  source = "git::https://github.com/worklytics/psoxy//infra/modules/source-token-external-todo?ref=v0.4.1"
+
+  source_id                         = each.key
+  host_cloud                        = "aws"
+  connector_specific_external_steps = each.value.external_token_todo
+  token_secret_id                   = aws_ssm_parameter.long-access-token-secret[each.key].name
+}
 # END LONG ACCESS AUTH CONNECTORS
 
 module "psoxy-hris" {
