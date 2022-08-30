1f040c60f865412d1dbc55a1dce63bbfa3293da0
davidfq <davidfq@gmail.com>
2022-08-30 19:31:34 +0200

    Re-structure project @eschultink CR
    It also decouples main script from "cli" stuff which
    is handled by commander lib

--- tools/psoxy-test/README.md
+++ tools/psoxy-test/README.md
@@ -7,7 +7,7 @@ Node.js based testing tool for Worklytics Psoxy, AWS and CGP deploys.
 [Node.js](version >=16) and [npm] (version >=8).
 
 1. `npm i` 
-2. `node psoxy-testing.js -u https://psoxy-url.com`
+2. `node tools/psoxy-test/cli.js -h`
 
 [Node.js]: https://nodejs.org/en/
 [npm]: https://www.npmjs.com
---
+++ tools/psoxy-test/index.js
@@ -0,0 +1,86 @@
+import aws from "./lib/aws.js";
+import gcp from "./lib/gcp.js";
+
+/**
+ * @typedef {Object} PsoxyResponse
+ * @property {string} error - Error message if any
+ * @property {number} status - HTTP request status code
+ * @property {Object} data - HTTP request body (JSON)
+ */
+
+/**
+ * Psoxy test call
+ *
+ * @param {Object} options
+ * @param {string} options.url - Psoxy URL to call
+ * @param {string} options.force - Force URL as AWS or GCP deploy
+ * @param {string} options.impersonate - User to impersonate (Google Workspace API)
+ * @param {string} options.token - Authorization token for GCP deploys
+ * @param {string} options.role - AWS role to assume when calling the Psoxy (ARN format)
+ * @param {boolean} options.skip - Whether to skip or not sanitization rules (only in DEV mode)
+ * @param {boolean} options.gzip - Add Gzip compression headers
+ * @param {boolean} options.verbose - Verbose ouput (default to console)
+ * @return {PsoxyResponse}
+ */
+export default async function (options = {}) {
+  const result = {};
+  let url;
+
+  try {
+    url = new URL(options.url);
+  } catch (err) {
+    result.error = `"${options.url}" is not a valid URL`;
+    return result;
+  }
+
+  const isAWS = aws.isAWS(url);
+  const isGCP = gcp.isGCP(url);
+  let test;
+
+  if (options.force && ["aws", "gcp"].includes(options.force.toLowerCase())) {
+    test = options.force === "aws" ? aws.test : gcp.test;
+  } else if (!isAWS && !isGCP) {
+    result.error = `"${options.url}" doesn't seem to be a valid endpoint: AWS or GCP`;
+    return result;
+  } else {
+    test = isAWS ? aws.test : gcp.test;
+  }
+
+  try {
+    const response = await test(options);
+
+    if (response.status === 200) {
+      const data = await response.json();
+
+      result.response = {
+        status: response.status,
+        data: JSON.stringify(data, undefined, 4),
+      };
+    } else {
+      const psoxyError = response.headers.get("x-psoxy-error");
+
+      if (psoxyError) {
+        switch (psoxyError) {
+          case "BLOCKED_BY_RULES":
+            result.error = "Blocked by rules error: make sure URL path is correct";
+            break;
+          case "CONNECTION_SETUP":
+            result.error =
+              "Connection setup error: make sure the data source is properly configured";
+            break;
+          case "API_ERROR":
+            result.error = "API error: call to data source failed";
+            break;
+          default:
+            result.error = psoxyError;
+        }
+      } else {
+        result.error = response.statusText;
+      }
+    }
+  } catch (err) {
+    result.error = err.message;
+  }
+
+  return result;
+}
