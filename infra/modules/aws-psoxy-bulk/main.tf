52f21a3e73adf846b7bb97063abc75836dd0ff36
Erik Schultink <erik@worklytics.co>
2022-08-30 08:35:33 -0700

    recover encryption/no public objects

--- infra/modules/aws-psoxy-bulk/main.tf
+++ infra/modules/aws-psoxy-bulk/main.tf
@@ -18,16 +18,16 @@ resource "random_string" "bucket_suffix" {
 module "psoxy_lambda" {
   source = "../aws-psoxy-lambda"
 
-  function_name        = "psoxy-${var.instance_id}"
-  handler_class        = "co.worklytics.psoxy.S3Handler"
-  timeout_seconds      = 600 # 10 minutes
-  memory_size_mb       = 512
-  path_to_config       = var.path_to_config
-  path_to_function_zip = var.path_to_function_zip
-  function_zip_hash    = var.function_zip_hash
-  aws_assume_role_arn  = var.aws_assume_role_arn
-  source_kind          = var.source_kind
-  parameters           = []
+  function_name         = "psoxy-${var.instance_id}"
+  handler_class         = "co.worklytics.psoxy.S3Handler"
+  timeout_seconds       = 600 # 10 minutes
+  memory_size_mb        = 512
+  path_to_config        = var.path_to_config
+  path_to_function_zip  = var.path_to_function_zip
+  function_zip_hash     = var.function_zip_hash
+  aws_assume_role_arn   = var.aws_assume_role_arn
+  source_kind           = var.source_kind
+  parameters            = []
   environment_variables = merge(
     var.environment_variables,
     {
@@ -41,22 +41,46 @@ resource "aws_s3_bucket" "input" {
   bucket = "psoxy-${var.instance_id}-${random_string.bucket_suffix.id}-input"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "input" {
+  bucket = aws_s3_bucket.input.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "input-block-public-access" {
   bucket = aws_s3_bucket.input.bucket
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_s3_bucket" "output" {
   bucket = "psoxy-${var.instance_id}-${random_string.bucket_suffix.id}-output"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "output" {
+  bucket = aws_s3_bucket.output.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 resource "aws_s3_bucket_public_access_block" "output-block-public-access" {
   bucket = aws_s3_bucket.output.bucket
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_lambda_permission" "allow_input_bucket" {
