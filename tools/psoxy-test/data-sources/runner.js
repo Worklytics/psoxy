991df3e72c088feed23ea0d39f5962ad5020fd80
davidfq <davidfq@gmail.com>
2022-09-16 12:56:52 +0200

    Remove `.env` file mechanism
    Use `options` object, same as "main" script,
    and export a named function for multiple calls

--- tools/psoxy-test/data-sources/runner.js
+++ tools/psoxy-test/data-sources/runner.js
@@ -1,53 +1,50 @@
 import psoxyTest from '../index.js';
 import chalk from 'chalk';
-import dotenv from 'dotenv';
 import spec from './spec.js';
 import { transformSpecWithResponse } from '../lib/utils.js';
-dotenv.config();
 
-const base = process.env.PSOXY_URL;
-const options = {
-  role: process.env.PSOXY_ROLE,
-  token: process.env.PSOXY_TOKEN,
-  impersonate: process.env.PSOXY_IMPERSONATE,
-  saveToFile: process.env.PSOXY_SAVE_TO_FILE === 'true',
-  verbose: process.env.PSOXY_VERBOSE === 'true',
-};
-
-(async function() {
-  const dataSource = spec[process.argv.slice(2)[0]];
-  if (!dataSource) {
+/**
+ * Run multiple psoxy test calls depending on options.dataSource spec
+ *
+ * @param {Object} options - see `../index.js`
+ * @returns {Object}
+ */
+async function callDataSourceEndpoints(options) {
+  const dataSourceSpec = spec[options.dataSource];
+  if (!dataSourceSpec) {
     console.error(chalk.bold.red('Unknown data source'));
     return;
   }
 
-  for (const endpoint of dataSource.endpoints) {
-    let result;
+  const results = {};
+
+  for (const endpoint of dataSourceSpec.endpoints) {
     let paramsString = '';
     if (endpoint.params) {
       const params = new URLSearchParams();
-      for(const [key, value] of Object.entries(endpoint.params)) {
+      for (const [key, value] of Object.entries(endpoint.params)) {
         params.append(key, value);
       }
       paramsString = `?${params.toString()}`;
     }
 
-    const url = base + endpoint.path + paramsString;
-    console.log(`${chalk.blue(dataSource.name)}, fetching ${chalk.blue(endpoint.name)}: ${url}`);
-    result = await psoxyTest({
+    const url = options.url + endpoint.path + paramsString;
+    console.log(`
+      ${chalk.blue(dataSourceSpec.name)}, fetching ${chalk.blue(endpoint.name)}: ${url}
+    `);
+
+    const result = await psoxyTest({
       ...options,
       url: url,
-    })
-
-    if (result?.error) {
-      console.error(chalk.bold.red(result.error));
-    } else if (result.status === 200) {
-      console.log(chalk.green('OK'));
-      console.log(result.data);
-    }
+    });
+    results[endpoint] = result;
 
     if (endpoint.refs) {
-      transformSpecWithResponse(dataSource, result.data);
+      transformSpecWithResponse(dataSourceSpec, result.data);
     }
   }
-})();
+
+  return results;
+}
+
+export { callDataSourceEndpoints };
