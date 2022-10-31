18d422a879635f09f28841561ae1a9262aae842f
Erik Schultink <erik@worklytics.co>
2022-10-31 16:24:06 -0700

    fix worklytics-connector-specs

--- infra/modules/worklytics-connector-specs/main.tf
+++ infra/modules/worklytics-connector-specs/main.tf
@@ -87,6 +87,7 @@ locals {
       oauth_scopes_needed : [
         "https://www.googleapis.com/auth/admin.reports.audit.readonly"
       ]
+      environment_variables : {}
       example_api_calls : [
         "/admin/reports/v1/activity/users/all/applications/meet"
       ]
