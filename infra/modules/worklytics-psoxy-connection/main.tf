5ed2bc75211700eae33aa922e273847481e24cd9
davidfq <davidfq@gmail.com>
2022-11-07 16:08:25 +0100

    Fix Worklytics web app URLs

--- infra/modules/worklytics-psoxy-connection/main.tf
+++ infra/modules/worklytics-psoxy-connection/main.tf
@@ -8,7 +8,7 @@ resource "local_file" "todo-worklytics-connection" {
   filename = "TODO ${var.todo_step} - connect ${local.instance_id} in Worklytics.md"
   content  = <<EOT
 Complete the following steps in Worklytics AFTER you have deployed the Psoxy instance for your connection:
-  1.  Visit https://intl.worklytics.co/#integrations (or login into Worklytics, and navigate to
+  1.  Visit https://intl.worklytics.co/analytics/integrations (or login into Worklytics, and navigate to
       Manage --> Data Connections)
   2.  Find the connector named "${var.display_name}", click "Add new connection"
   3.  Copy and paste `${var.psoxy_endpoint_url}` as the value for "Psoxy Base URL".
