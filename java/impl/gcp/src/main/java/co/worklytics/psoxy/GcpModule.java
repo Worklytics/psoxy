c30a10ceb40b6b82c8fab9412e352185644b4b11
Andrés Pérez <andres@worklytics.co>
2022-11-10 20:57:37 +0100

    Added SecretManagerConfigService

--- java/impl/gcp/src/main/java/co/worklytics/psoxy/GcpModule.java
+++ java/impl/gcp/src/main/java/co/worklytics/psoxy/GcpModule.java
@@ -2,15 +2,52 @@ package co.worklytics.psoxy;
 
 
 import co.worklytics.psoxy.gateway.ConfigService;
+import co.worklytics.psoxy.gateway.impl.CompositeConfigService;
 import co.worklytics.psoxy.gateway.impl.EnvVarsConfigService;
-import dagger.Binds;
+import com.google.cloud.ServiceOptions;
 import dagger.Module;
+import dagger.Provides;
+
+import javax.inject.Named;
+import javax.inject.Singleton;
+import java.time.Duration;
 
 /**
  * defines how to fulfill dependencies that need platform-specific implementations for GCP platform
  */
 @Module
 public interface GcpModule {
+
+    // https://cloud.google.com/functions/docs/configuring/env-var#newer_runtimes
+    enum RuntimeEnvironmentVariables {
+        K_SERVICE
+    }
+
+
+    static String asParameterStoreNamespace(String functionName) {
+        return functionName.toUpperCase().replace("-", "_");
+    }
+
+    // global parameters
+    // singleton to be reused in cloud function container
+    @Provides
+    @Named("Global")
+    @Singleton
+    static SecretManagerConfigService secretManagerConfigService() {
+        // Global don't change that often, use longer TTL
+        return new SecretManagerConfigService(null, ServiceOptions.getDefaultProjectId(), Duration.ofMinutes(20));
+    }
+
+    // parameters scoped to function
+    // singleton to be reused in cloud function container
+    @Provides
+    @Singleton
+    static SecretManagerConfigService functionSecretManagerConfigService() {
+        String namespace =
+                asParameterStoreNamespace(System.getenv(RuntimeEnvironmentVariables.K_SERVICE.name()));
+        // Namespaced params may change often (refresh tokens), use shorter TTL
+        return new SecretManagerConfigService(namespace, ServiceOptions.getDefaultProjectId(), Duration.ofMinutes(5));
+    }
 
     /**
      * in GCP cloud function, we should be able to configure everything via env vars; either
@@ -19,5 +56,19 @@ public interface GcpModule {
      * @see "https://cloud.google.com/functions/docs/configuring/env-var"
      * @see "https://cloud.google.com/functions/docs/configuring/secrets"
      */
-    @Binds ConfigService configService(EnvVarsConfigService envVarsConfigService);
+    @Provides
+    static ConfigService configService(EnvVarsConfigService envVarsConfigService,
+                                       @Named("Global") SecretManagerConfigService globalSecretManagerConfigService,
+                                       SecretManagerConfigService functionScopedSecretManagerConfigService) {
+
+        CompositeConfigService parameterStoreConfigHierarchy = CompositeConfigService.builder()
+                .fallback(globalSecretManagerConfigService)
+                .preferred(functionScopedSecretManagerConfigService)
+                .build();
+
+        return CompositeConfigService.builder()
+                .fallback(parameterStoreConfigHierarchy)
+                .preferred(envVarsConfigService)
+                .build();
+    }
 }
---
+++ java/impl/gcp/src/main/java/co/worklytics/psoxy/SecretManagerConfigService.java
@@ -0,0 +1,128 @@
+package co.worklytics.psoxy;
+
+import co.worklytics.psoxy.gateway.ConfigService;
+import com.google.cloud.secretmanager.v1.*;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.common.util.concurrent.UncheckedExecutionException;
+import com.google.protobuf.ByteString;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+@Log
+@RequiredArgsConstructor
+public class SecretManagerConfigService implements ConfigService {
+
+    final String namespace;
+
+    final String projectId;
+
+    final Duration defaultTtl;
+    private volatile LoadingCache<String, String> cache;
+    private final Object $writeLock = new Object[0];
+
+    private final String NEGATIVE_VALUE = "##NO_VALUE##";
+
+    private LoadingCache<String, String> getCache() {
+        if (this.cache == null) {
+            synchronized ($writeLock) {
+                if (this.cache == null) {
+                    this.cache = CacheBuilder.newBuilder()
+                            .maximumSize(100)
+                            .expireAfterWrite(defaultTtl.getSeconds(), TimeUnit.SECONDS)
+                            .recordStats()
+                            .build(new CacheLoader<>() {
+                                @Override
+                                public String load(String key) throws IOException {
+                                    try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+                                        SecretVersionName secretVersionName = SecretVersionName.of(projectId, key, "latest");
+
+                                        // Access the secret version.
+                                        AccessSecretVersionResponse response = client.accessSecretVersion(secretVersionName);
+
+                                        return response.getPayload().getData().toStringUtf8();
+                                    }
+                                }
+                            });
+                }
+            }
+        }
+
+        return cache;
+    }
+
+    @Override
+    public boolean supportsWriting() {
+        return true;
+    }
+
+    @Override
+    public void putConfigProperty(ConfigProperty property, String value) {
+        String key = parameterName(property);
+        try {
+            try (SecretManagerServiceClient client = SecretManagerServiceClient.create()) {
+                // first in the local cache so other threads get the most recent
+                getCache().put(key, value);
+
+                SecretPayload payload =
+                        SecretPayload.newBuilder()
+                                .setData(ByteString.copyFrom(value.getBytes()))
+                                .build();
+
+                // Add the secret version.
+                SecretVersion version = client.addSecretVersion(key, payload);
+
+                log.info(String.format("Property: %s, stored version %s", key, version.getName()));
+            }
+        } catch (IOException e) {
+            log.log(Level.SEVERE, "Could not store property " + key, e);
+        }
+    }
+
+    @Override
+    public String getConfigPropertyOrError(ConfigProperty property) {
+        return getConfigPropertyAsOptional(property)
+                .orElseThrow(() -> new Error("Proxy misconfigured; no value for " + property));
+    }
+
+    private String parameterName(ConfigProperty property) {
+        if (StringUtils.isBlank(this.namespace)) {
+            return property.name();
+        } else {
+            return String.join("_", this.namespace, property.name());
+        }
+    }
+
+    @Override
+    public Optional<String> getConfigPropertyAsOptional(ConfigProperty property) {
+        String paramName = null;
+        try {
+            paramName = parameterName(property);
+            String value = getCache().get(paramName);
+            // useful for debugging to check if cache works as expected
+            // log.info(getCache().stats().toString());
+            if (NEGATIVE_VALUE.equals(value)) {
+                // Optional is common, do not log, just for testing/debugging purposes
+                // log.log(Level.WARNING, String.format("Parameter not found %s", paramName));
+                return Optional.empty();
+            } else {
+                return Optional.of(value);
+            }
+        } catch (ExecutionException ee) {
+            Throwable cause = ee.getCause();
+            throw new IllegalStateException(String.format("failed to get config value: %s", paramName), cause);
+        } catch (UncheckedExecutionException uee) {
+            // unchecked?
+            throw new IllegalStateException(String.format("failed to get config value: %s", paramName), uee.getCause());
+        }
+    }
+}
