2e1dccfd32afbaa151a8442e890b88ad366f70a0
Erik Schultink <erik@worklytics.co>
2023-01-05 19:40:21 -0800

    better names to bash variables

--- infra/modules/psoxy-package/build.sh
+++ infra/modules/psoxy-package/build.sh
@@ -4,20 +4,24 @@
 set -e
 # psoxy build script to be invoked from Terraform 'external' data resource
 # NOTE:
+JAVA_SOURCE_ROOT=$1
+IMPLEMENTATION=$2 # expected to be 'aws', 'gcp', etc ...
+PATH_TO_DEPLOYMENT_JAR=$3
+
 TERRAFORM_CONFIG_PATH=`pwd`
 LOG_FILE=/tmp/psoxy-package.`date +%Y%m%d'T'%H%M%S`.log
 
 ln -sf ${LOG_FILE} ${TERRAFORM_CONFIG_PATH}/last-build.log
 
-cd $1/gateway-core
+cd ${JAVA_SOURCE_ROOT}/gateway-core
 mvn package install > ${LOG_FILE} 2>&1
 
-cd $1/core
+cd ${JAVA_SOURCE_ROOT}/core
 mvn package install > ${LOG_FILE} 2>&1
 
-cd $1/impl/$2
+cd ${JAVA_SOURCE_ROOT}/impl/${IMPLEMENTATION}
 mvn package >> ${LOG_FILE} 2>&1
 
 # output back to Terraform
-OUTPUT_JSON="{\"path_to_deployment_jar\": \"$3\"}"
+OUTPUT_JSON="{\"path_to_deployment_jar\": \"${PATH_TO_DEPLOYMENT_JAR}\"}"
 echo "$OUTPUT_JSON"
