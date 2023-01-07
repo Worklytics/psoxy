ba3d6c8110f2e24a93d7ae9b87b6aa10ff217299
Erik Schultink <erik@worklytics.co>
2023-01-06 16:11:27 -0800

    update READMES, example variables, dev examples

--- infra/examples-dev/gcp-google-workspace/main.tf
+++ infra/examples-dev/gcp-google-workspace/main.tf
@@ -22,24 +22,16 @@ terraform {
   #  }
 }
 
-# NOTE: if you don't have perms to provision a GCP project in your billing account, you can have
-# someone else create one and than import it:
-#  `terraform import google_project.psoxy-project your-psoxy-project-id`
-# either way, we recommend the project be used exclusively to host psoxy instances corresponding to
-# a single worklytics account
-resource "google_project" "psoxy-project" {
-  name            = "Psoxy%{if var.environment_name != ""} - ${var.environment_name}%{endif}"
-  project_id      = var.gcp_project_id
-  billing_account = var.gcp_billing_account_id
-  folder_id       = var.gcp_folder_id # if project is at top-level of your GCP organization, rather than in a folder, comment this line out
-  # org_id          = var.gcp_org_id # if project is in a GCP folder, this value is implicit and this line should be commented out
+# NOTE: google user executing terraform must be able to provision relevant infra in this GCP project
+data "google_project" "psoxy-project" {
+  project_id = var.gcp_project_id
 }
 
 module "psoxy-gcp-google-workspace" {
   source = "../../modular-examples/gcp-google-workspace"
   # source = "git::https://github.com/worklytics/psoxy//infra/modular-examples/aws-google-workspace?ref=v0.4.9"
 
-  gcp_project_id                = google_project.psoxy-project.project_id
+  gcp_project_id                = data.google_project.psoxy-project.project_id
   environment_name              = var.environment_name
   worklytics_sa_emails          = var.worklytics_sa_emails
   connector_display_name_suffix = var.connector_display_name_suffix
@@ -52,10 +44,6 @@ module "psoxy-gcp-google-workspace" {
   custom_bulk_connectors        = var.custom_bulk_connectors
   google_workspace_example_user = var.google_workspace_example_user
   general_environment_variables = var.general_environment_variables
-
-  depends_on = [
-    google_project.psoxy-project
-  ]
 }
 
 moved {
