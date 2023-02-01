51dd00561980c19dc4d70e510382818cc24037c9
Andrés Pérez <andres@worklytics.co>
2023-02-01 14:44:22 +0100

    Drop code used as part of old script execution

--- infra/modules/aws-cognito-identity-cli/main.tf
+++ infra/modules/aws-cognito-identity-cli/main.tf
@@ -1,21 +1,3 @@
-#resource "null_resource" "identity-command" {
-#  for_each = var.login-ids
-#
-#  triggers = {
-#    always_run = "${timestamp()}"
-#  }
-#  provisioner "local-exec" {
-#    command = "bash ${path.module}/addIdentity.sh ${var.identity_pool_id} ${each.value} ${var.aws_region} ${var.aws_role} ${each.key} ${path.module}"
-#  }
-#}
-#
-#data "local_file" "identity-result" {
-#  for_each = var.login-ids
-#
-#  filename   = "${path.module}/cognito_identity_${each.key}.json"
-#  depends_on = [null_resource.identity-command]
-#}
-
 data "external" "identity-result" {
   for_each = var.login-ids
 
