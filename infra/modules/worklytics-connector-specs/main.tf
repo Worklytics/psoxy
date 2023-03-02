8a1305330e6e45091bb37e363ae926d6416a41f9
davidfq <davidfq@gmail.com>
2023-03-02 17:29:03 +0100

    Fix missing URL reference to Asana docs

--- infra/modules/worklytics-connector-specs/main.tf
+++ infra/modules/worklytics-connector-specs/main.tf
@@ -214,8 +214,9 @@ locals {
       ],
       external_token_todo : <<EOT
   1. Create a [Service Account User + token](https://asana.com/guide/help/premium/service-accounts)
-     or a sufficiently [Personal Access Token]() for a sufficiently privileged user (who can see all
-     the workspaces/teams/projects/tasks you wish to import to Worklytics via this connection).
+    or a sufficiently [Personal Access Token](https://developers.asana.com/docs/personal-access-token)
+    for a sufficiently privileged user (who can see all the workspaces/teams/projects/tasks you wish to
+    import to Worklytics via this connection).
   2. Update the content of PSOXY_ASANA_ACCESS_TOKEN variable with the previous token value obtained
 EOT
     }
