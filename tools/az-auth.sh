e92d91aa87afe709d91a0e736c01ee238d92c13c
Erik Schultink <erik@worklytics.co>
2023-03-20 15:59:17 -0700

    remove dep on yz/jq

--- tools/az-auth.sh
+++ tools/az-auth.sh
@@ -5,12 +5,8 @@ GREEN='\e[0;32m'
 BLUE='\e[0;34m'
 NC='\e[0m' # No Color
 
-if yj -v &> /dev/null  && jq --version &> /dev/null ; then
-  TENANT_ID=`cat terraform.tfvars | yj -c | jq -r .msft_tenant_id`
-  TENANT_ID_CLAUSE="--tenant ${TENANT_ID}"
-  printf "Azure (Microsoft 365) tenant will be forced to ${GREEN}${TENANT_ID}${NC}, parsed from your ${BLUE}terraform.tfvars${NC}. If you pick user from different tenant, auth will fail.\r\n"
-else
-  TENANT_ID_CLAUSE=""
-fi
+TENANT_ID=`echo "var.msft_tenant_id" | terraform console | tr -d '\"'`
+TENANT_ID_CLAUSE="--tenant ${TENANT_ID}"
+printf "Azure (Microsoft 365) tenant will be forced to ${GREEN}${TENANT_ID}${NC}, parsed from your ${BLUE}terraform.tfvars${NC}. If you pick user from different tenant, auth will fail.\r\n"
 
 az login --allow-no-subscriptions $TENANT_ID_CLAUSE
