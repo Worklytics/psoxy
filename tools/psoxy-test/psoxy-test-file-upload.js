6e1f44bd4a268ab79ad601f9ec0daac13e38149a
davidfq <davidfq@gmail.com>
2023-04-18 07:49:00 +0200

    Fix output bucket reference

--- tools/psoxy-test/psoxy-test-file-upload.js
+++ tools/psoxy-test/psoxy-test-file-upload.js
@@ -89,7 +89,7 @@ async function testGCP(options, logger) {
   logger.success(`File uploaded -> ${uploadResult.mediaLink}`);
   logger.verbose('Upload result:', { additional: uploadResult });
 
-  const parsedBucketOutputOption = parseBucketOption(options.input);
+  const parsedBucketOutputOption = parseBucketOption(options.output);
   const outputBucket = parsedBucketOutputOption.bucket;
   const outputPath = parsedBucketOutputOption.path;
   const outputKey = outputPath + filenameWithTimestamp;
