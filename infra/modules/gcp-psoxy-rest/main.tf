96a52f6e365aab9f412a17f35868a41611eb3829
Erik Schultink <erik@worklytics.co>
2023-04-20 13:47:40 -0700

    fix SA user in REST case

--- infra/modules/gcp-psoxy-rest/main.tf
+++ infra/modules/gcp-psoxy-rest/main.tf
@@ -37,6 +37,32 @@ locals {
   }
 }
 
+data "google_client_openid_userinfo" "me" {
+
+}
+
+locals {
+  # hacky way to determine if Terraform running as a service account or not
+  tf_is_service_account = endswith(data.google_client_openid_userinfo.me.email, "iam.gserviceaccount.com")
+
+  tf_qualifier          = local.tf_is_service_account ? "serviceAccount:" : "user:"
+  tf_principal          = "${local.tf_qualifier}${data.google_client_openid_userinfo.me.email}"
+}
+
+data "google_service_account" "function" {
+  account_id = var.service_account_email
+}
+
+
+# to provision Cloud Function, TF must be able to act as the service account that the function will
+# run as
+resource "google_service_account_iam_member" "act_as" {
+  member             = local.tf_principal
+  role               = "roles/iam.serviceAccountUser"
+  service_account_id = data.google_service_account.function.id
+}
+
+
 resource "google_cloudfunctions_function" "function" {
   name        = "${var.environment_id_prefix}${var.instance_id}"
   description = "Psoxy Connector - ${var.source_kind}"
@@ -79,7 +105,8 @@ resource "google_cloudfunctions_function" "function" {
   }
 
   depends_on = [
-    google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret
+    google_secret_manager_secret_iam_member.grant_sa_accessor_on_secret,
+    google_service_account_iam_member.act_as
   ]
 }
 
