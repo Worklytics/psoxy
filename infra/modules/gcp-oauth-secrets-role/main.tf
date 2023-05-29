1c313a4b964d08fb32f525882f0d5976d61612e6
aperez-worklytics <75276364+aperez-worklytics@users.noreply.github.com>
2023-05-29 18:30:28 +0200

    Update infra/modules/gcp-oauth-secrets-role/main.tf
    Co-authored-by: Erik Schultink <eschultink@users.noreply.github.com>

--- infra/modules/gcp-oauth-secrets-role/main.tf
+++ infra/modules/gcp-oauth-secrets-role/main.tf
@@ -2,7 +2,7 @@ resource "google_project_iam_custom_role" "secret_manager_updater" {
   project     = var.project_id
   role_id     = "SecretManagerAccess"
   title       = "Access for updating and reading secrets"
-  description = "Access for updating and reading secrets because updating secrets are only available as roles/secretmanager.admin"
+  description = "Role to grant on secret that is to be managed by a Psoxy instance (cloud function); subset of roles/secretmanager.admin, to support reading/updating the secret"
 
   permissions = [
     "resourcemanager.projects.get",
