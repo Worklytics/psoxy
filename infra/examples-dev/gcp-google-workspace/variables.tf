bbb01b5eb63472b7d3af89160380f904a2d3a178
Erik Schultink <erik@worklytics.co>
2023-06-01 19:06:42 -0700

    hardlinks instead of symlinks

--- infra/examples-dev/gcp-google-workspace/variables.tf
+++ infra/examples-dev/gcp-google-workspace/variables.tf
@@ -1,1 +1,180 @@
-examples/gcp-google-workspace/variables.tf
+variable "gcp_project_id" {
+  type        = string
+  description = "id of GCP project that will host psoxy instance; must exist"
+}
+
+variable "gcp_terraform_sa_account_email" {
+  type        = string
+  description = "Email of GCP service account that will be used to provision GCP resources. Leave 'null' to use application default for you environment."
+  default     = null
+
+  validation {
+    condition     = var.gcp_terraform_sa_account_email == null || can(regex(".*@.*\\.iam\\.gserviceaccount\\.com$", var.gcp_terraform_sa_account_email))
+    error_message = "The gcp_terraform_sa_account_email value should be a valid GCP service account email address."
+  }
+}
+
+variable "environment_name" {
+  type        = string
+  description = "qualifier to append to name of project that will host your psoxy instance"
+  default     = ""
+}
+
+variable "gcp_org_id" {
+  type        = string
+  description = "DEPRECATED; IGNORED; your GCP organization ID"
+  default     = null
+}
+
+variable "gcp_folder_id" {
+  type        = string
+  description = "DEPRECATED; IGNORED; optionally, a folder into which to provision it"
+  default     = null
+}
+
+variable "gcp_billing_account_id" {
+  type        = string
+  description = "DEPRECATED; IGNORED; billing account ID; needed to create the project"
+  default     = null
+}
+
+variable "worklytics_sa_emails" {
+  type        = list(string)
+  description = "service accounts for your organization's Worklytics instances (list supported for test/dev scenarios)"
+}
+
+variable "connector_display_name_suffix" {
+  type        = string
+  description = "suffix to append to display_names of connector SAs; helpful to distinguish between various ones in testing/dev scenarios"
+  default     = ""
+}
+
+variable "psoxy_base_dir" {
+  type        = string
+  description = "the path where your psoxy repo resides"
+
+  validation {
+    condition     = can(regex(".*\\/$", var.psoxy_base_dir))
+    error_message = "The psoxy_base_dir value should end with a slash."
+  }
+  validation {
+    condition     = can(regex("^[^~].*$", var.psoxy_base_dir))
+    error_message = "The psoxy_base_dir value should be absolute path (not start with ~)."
+  }
+}
+
+variable "force_bundle" {
+  type        = bool
+  description = "whether to force build of deployment bundle, even if it already exists for this proxy version"
+  default     = false
+}
+
+variable "general_environment_variables" {
+  type        = map(string)
+  description = "environment variables to add for all connectors"
+  default     = {}
+}
+
+variable "pseudonymize_app_ids" {
+  type        = string
+  description = "if set, will set value of PSEUDONYMIZE_APP_IDS environment variable to this value for all sources"
+  default     = true
+}
+
+variable "gcp_region" {
+  type        = string
+  description = "Region in which to provision GCP resources, if applicable"
+  default     = "us-central1"
+}
+
+variable "replica_regions" {
+  type        = list(string)
+  description = "List of regions in which to replicate secrets."
+  default = [
+    "us-central1",
+    "us-west1",
+  ]
+}
+
+variable "enabled_connectors" {
+  type        = list(string)
+  description = "list of ids of connectors to enabled; see modules/worklytics-connector-specs"
+
+  default = [
+    "asana",
+    "gdirectory",
+    "gcal",
+    "gmail",
+    "gdrive",
+    "google-chat",
+    "google-meet",
+    "hris",
+    "slack-discovery-api",
+    "zoom",
+  ]
+}
+
+variable "bulk_input_expiration_days" {
+  type        = number
+  description = "**alpha** Number of days after which objects in the bucket will expire"
+  default     = 30
+}
+
+variable "bulk_sanitized_expiration_days" {
+  type        = number
+  description = "**alpha** Number of days after which objects in the bucket will expire"
+  default     = 720
+}
+
+variable "custom_bulk_connectors" {
+  type = map(object({
+    source_kind = string
+    rules = object({
+      pseudonymFormat       = optional(string, "URL_SAFE_TOKEN")
+      columnsToRedact       = optional(list(string))
+      columnsToInclude      = optional(list(string))
+      columnsToPseudonymize = optional(list(string))
+      columnsToDuplicate    = optional(map(string))
+      columnsToRename       = optional(map(string))
+    })
+    settings_to_provide = optional(map(string), {})
+  }))
+  description = "specs of custom bulk connectors to create"
+
+  default = {
+    #    "custom-survey" = {
+    #      source_kind = "survey"
+    #      rules       = {
+    #        columnsToRedact       = []
+    #        columnsToPseudonymize = [
+    #          "employee_id", # primary key
+    #          # "employee_email", # if exists
+    #        ]
+    #      }
+    #    }
+  }
+}
+
+variable "non_production_connectors" {
+  type        = list(string)
+  description = "connector ids in this list will be in development mode (not for production use"
+  default     = []
+}
+
+variable "google_workspace_example_user" {
+  type        = string
+  description = "User to impersonate for Google Workspace API calls (null for none)"
+  default     = null
+}
+
+variable "google_workspace_example_admin" {
+  type        = string
+  description = "user to impersonate for Google Workspace API calls (null for value of `google_workspace_example_user`)"
+  default     = null # will failover to user
+}
+
+variable "salesforce_domain" {
+  type        = string
+  description = "Domain of the Salesforce to connect to (only required if using Salesforce connector). To find your My Domain URL, from Setup, in the Quick Find box, enter My Domain, and then select My Domain"
+  default     = ""
+}
