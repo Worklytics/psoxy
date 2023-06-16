b0f4e352f5d9173fa592784f10fb38e91b0f29e1
Erik Schultink <erik@worklytics.co>
2023-06-16 13:45:38 -0700

    weird stray backticks

--- infra/examples-dev/aws-all/main.tf
+++ infra/examples-dev/aws-all/main.tf
@@ -89,8 +89,8 @@ module "psoxy" {
   environment_name               = var.environment_name
   aws_account_id                 = var.aws_account_id
   aws_assume_role_arn            = var.aws_assume_role_arn # role that can test the instances (lambdas)
-  aws_region                     = var.aws_region`
-  aws_ssm_param_root_path        = var.aws_ssm_param_root_path`
+  aws_region                     = var.aws_region
+  aws_ssm_param_root_path        = var.aws_ssm_param_root_path
   psoxy_base_dir                 = var.psoxy_base_dir
   install_test_tool              = var.install_test_tool
   provision_testing_infra        = var.provision_testing_infra
