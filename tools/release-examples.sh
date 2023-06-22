fac4a72b1de6cf0c7f6af358ea3a920632722f76
Erik Schultink <erik@worklytics.co>
2023-06-22 11:22:56 -0700

    examples release script that correctly fixes module references

--- tools/release-examples.sh
+++ tools/release-examples.sh
@@ -1,21 +1,66 @@
 #!/bin/bash
 
+# colors
+RED='\e[0;31m'
+BLUE='\e[0;34m'
+NC='\e[0m' # No Color
+
 # Usage:
 # ./release-examples.sh <path-to-example> <path-to-aws-repo>
-# ./release-examples.sh ../infra/examples-dev/aws-all /Users/erik/psoxy-example-aws
-# ./release-examples.sh ../infra/examples-dev/gcp /Users/erik/psoxy-example-gcp
+# ./release-examples.sh v0.4.25 ~/code/psoxy/infra/examples-dev/aws-all ~/psoxy-example-aws
+# ./release-examples.sh v0.4.25 ~/code/psoxy/infra/examples-dev/gcp ~/psoxy-example-gcp
 
-DEV_EXAMPLE_PATH=$1
-EXAMPLE_TEMPLATE_REPO=$2
+RELEASE_TAG=$1
+DEV_EXAMPLE_PATH=$2
+EXAMPLE_TEMPLATE_REPO=$3
+
+if [ -z "$RELEASE_TAG" ]; then
+  printf "${RED}No arguments passed.${NC}\n"
+  printf "Usage:\n"
+  printf "  ./release-examples.sh <release-tag> <path-to-example> <path-to-aws-repo>\n"
+  printf "  ./release-examples.sh v0.4.25 ~/code/psoxy/infra/examples-dev/aws-all ~/psoxy-example-aws\n"
+  printf "  ./release-examples.sh v0.4.25 ~/code/psoxy/infra/examples-dev/gcp ~/psoxy-example-gcp\n"
+  exit 1
+fi
 
 FILES_TO_COPY=("main.tf" "variables.tf" "google-workspace.tf" "google-workspace-variables.tf" "msft-365.tf" "msft-365-variables.tf")
 
+set -e  # exit on error
+
+cd "$EXAMPLE_TEMPLATE_REPO"
+CURRENT_BRANCH=$(git branch --show-current)
+if [ "$CURRENT_BRANCH" != "main" ]; then
+  printf "${RED}Current branch is not main. Please checkout main branch and try again.${NC}\n"
+  exit 1
+fi
+
+BRANCH_STATUS=$(git status --porcelain)
+if [ -n "$BRANCH_STATUS" ]; then
+  printf "${RED}Current status of 'main' branch is not clean. Please commit or stash your changes and try again.${NC}\n"
+  exit 1
+fi
+
+git checkout -b "rc-${RELEASE_TAG}"
+
+cd -
 for file in "${FILES_TO_COPY[@]}"
 do
   cp -f ${DEV_EXAMPLE_PATH}/${file} ${EXAMPLE_TEMPLATE_REPO}/${file}
+
+  # uncomment Terraform module remotes
+  sed -i .bck 's/^\(.*\)# source = "git::\(.*\)"/\1source = "git::\2"/' "${EXAMPLE_TEMPLATE_REPO}/${file}"
+
+  # remove references to local modules
+  sed -i .bck '/source = "..\/..\/modules\/[^"]*"/d' "${EXAMPLE_TEMPLATE_REPO}/${file}"
 done
+
 
 cp -f ./init-example.sh ${EXAMPLE_TEMPLATE_REPO}/init
 cp -f ./check-prereqs.sh ${EXAMPLE_TEMPLATE_REPO}/check-prereqs
 chmod +x ${EXAMPLE_TEMPLATE_REPO}/init
 chmod +x ${EXAMPLE_TEMPLATE_REPO}/check-prereqs
+
+cd "$EXAMPLE_TEMPLATE_REPO"
+rm *.bck
+git commit -a -m "Update example to ${RELEASE_TAG}"
+cd -
