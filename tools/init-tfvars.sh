874d40151d8721a0b7018e955d206a935c9d2745
Erik Schultink <erik@worklytics.co>
2023-06-23 05:57:53 -0700

    conditional init of google_workspace variables

--- tools/init-tfvars.sh
+++ tools/init-tfvars.sh
@@ -88,35 +88,51 @@ if test $GOOGLE_PROVIDER_COUNT -ne 0; then
     printf "# GCP project in which required infrastructure will be provisioned\n" >> $TFVARS_FILE
     printf "#  - if you're deploying to AWS, you can omit this value\n" >> $TFVARS_FILE
     GCP_PROJECT_ID=$(gcloud config get project)
-    printf "gcp_project_id=\"${GCP_PROJECT_ID}\"\n\n" >> $TFVARS_FILE
-    printf "\tgcp_project_id=${BLUE}\"${GCP_PROJECT_ID}\"${NC}\n"
 
-    printf "# GCP project in which OAuth clients for Google Workspace connectors will be provisioned\n" >> $TFVARS_FILE
-    printf "#  - if you're not connecting to Google Workspace data sources, you can omit this value\n" >> $TFVARS_FILE
-    printf "google_workspace_gcp_project_id=\"${GCP_PROJECT_ID}\"\n\n" >> $TFVARS_FILE
-    printf "\tgoogle_workspace_gcp_project_id=${BLUE}\"${GCP_PROJECT_ID}\"${NC}\n"
+    [[ -f variables.tf ]] && grep -q '^variable "gcp_project_id"' variables.tf
+    if [[ $? -eq 0 ]]; then
+      printf "gcp_project_id=\"${GCP_PROJECT_ID}\"\n\n" >> $TFVARS_FILE
+      printf "\tgcp_project_id=${BLUE}\"${GCP_PROJECT_ID}\"${NC}\n"
+    fi
 
     # tenant SA emails
-    printf "# GCP service account emails in the list below will be allowed to invoke your proxy instances\n" >> $TFVARS_FILE
-    printf "#  - if you're deploying to AWS rather than GCP, you can omit this value\n" >> $TFVARS_FILE
-    printf "#  - for initial testing/deployment, it can be empty list; it needs to be filled only once you're ready to authorize Worklytics to access your data\n" >> $TFVARS_FILE
-    printf "worklytics_sa_emails=[\n" >> $TFVARS_FILE
-    printf "  # put 'Service Account Email' value here, which you can obtain from Worklytics ( https://intl.worklytics.co/analytics/integrations/configuration )\n" >> $TFVARS_FILE
-    printf "]\n\n" >> $TFVARS_FILE
+    [[ -f variables.tf ]] && grep -q '^variable "worklytics_sa_emails"' variables.tf
+    if [[ $? -eq 0 ]]; then
+      printf "# GCP service account emails in the list below will be allowed to invoke your proxy instances\n" >> $TFVARS_FILE
+      printf "#  - if you're deploying to AWS rather than GCP, you can omit this value\n" >> $TFVARS_FILE
+      printf "#  - for initial testing/deployment, it can be empty list; it needs to be filled only once you're ready to authorize Worklytics to access your data\n" >> $TFVARS_FILE
+      printf "worklytics_sa_emails=[\n" >> $TFVARS_FILE
+      printf "  # put 'Service Account Email' value here, which you can obtain from Worklytics ( https://intl.worklytics.co/analytics/integrations/configuration )\n" >> $TFVARS_FILE
+      printf "]\n\n" >> $TFVARS_FILE
+    fi
 
-    # example user for Google Workspace
-    printf "# Google Workspace example user \n" >> $TFVARS_FILE
-    printf "#  - this is used to aid testing of Google Workspace connectors against a real account (eg, your own); if you're not using those, it can be omitted\n" >> $TFVARS_FILE
-    GOOGLE_WORKSPACE_EXAMPLE_USER=$(gcloud config get account)
-    printf "google_workspace_example_user=\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"\n\n" >> $TFVARS_FILE
-    printf "\tgoogle_workspace_example_user=${BLUE}\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"${NC}\n"
+    # init google workspace variables if file exists OR the variables are in the main variables.tf file
+    # (google_workspace_gcp_project_id not in all legacy examples)
+    [[ -f google-workspace-variables.tf ]] || grep -q '^variable "google_workspace_gcp_project_id"' variables.tf
+    if [[ $? -eq 0 ]]; then
+      printf "# GCP project in which OAuth clients for Google Workspace connectors will be provisioned\n" >> $TFVARS_FILE
+      printf "#  - if you're not connecting to Google Workspace data sources, you can omit this value\n" >> $TFVARS_FILE
+      printf "google_workspace_gcp_project_id=\"${GCP_PROJECT_ID}\"\n\n" >> $TFVARS_FILE
+      printf "\tgoogle_workspace_gcp_project_id=${BLUE}\"${GCP_PROJECT_ID}\"${NC}\n"
+    fi
 
-        # example admin for Google Workspace
-    printf "# Google Workspace example admin \n" >> $TFVARS_FILE
-    printf "#  - this is used to aid testing of Google Workspace connectors against a real account, in cases where an admin is explicitly required\n" >> $TFVARS_FILE
-    GOOGLE_WORKSPACE_EXAMPLE_USER=$(gcloud config get account)
-    printf "google_workspace_example_admin=\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"\n\n" >> $TFVARS_FILE
-    printf "\tgoogle_workspace_example_admin=${BLUE}\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"${NC}\n"
+    # init google workspace variables if file exists OR the variables are in the main variables.tf file
+    [[ -f google-workspace-variables.tf ]] || grep -q '^variable "google_workspace_example_user"' variables.tf
+    if [[ $? -eq 0 ]]; then
+      # example user for Google Workspace
+      printf "# Google Workspace example user \n" >> $TFVARS_FILE
+      printf "#  - this is used to aid testing of Google Workspace connectors against a real account (eg, your own); if you're not using those, it can be omitted\n" >> $TFVARS_FILE
+      GOOGLE_WORKSPACE_EXAMPLE_USER=$(gcloud config get account)
+      printf "google_workspace_example_user=\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"\n\n" >> $TFVARS_FILE
+      printf "\tgoogle_workspace_example_user=${BLUE}\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"${NC}\n"
+
+      # example admin for Google Workspace
+      printf "# Google Workspace example admin \n" >> $TFVARS_FILE
+      printf "#  - this is used to aid testing of Google Workspace connectors against a real account, in cases where an admin is explicitly required\n" >> $TFVARS_FILE
+      GOOGLE_WORKSPACE_EXAMPLE_USER=$(gcloud config get account)
+      printf "google_workspace_example_admin=\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"\n\n" >> $TFVARS_FILE
+      printf "\tgoogle_workspace_example_admin=${BLUE}\"${GOOGLE_WORKSPACE_EXAMPLE_USER}\"${NC}\n"
+    fi
   else
     printf "${RED}gcloud not available${NC}\n"
   fi
@@ -134,18 +150,22 @@ if test $AZUREAD_PROVIDER_COUNT -ne 0; then
   printf "AzureAD provider in Terraform configuration. Initializing variables it requires ...\n"
   if az --version &> /dev/null ; then
 
-    printf "# Azure AD Apps (Microsoft API Clients) will be provisioned in the following tenant to access your Microsoft 365 data\n" >> $TFVARS_FILE
-    printf "#  - this should be the ID of your Microsoft 365 organization (tenant)\n" >> $TFVARS_FILE
-    printf "#  - if you're not connecting to Microsoft 365 data sources, you can omit this value\n" >> $TFVARS_FILE
-    MSFT_TENANT_ID=$(az account show --query tenantId --output tsv)
-    printf "msft_tenant_id=\"${MSFT_TENANT_ID}\"\n\n" >> $TFVARS_FILE
-    printf "\tmsft_tenant_id=${BLUE}\"${MSFT_TENANT_ID}\"${NC}\n"
+    # init msft 365 variables if file exists OR the variables are in the main variables.tf file
+    [[ -f msft-365-variables.tf ]] || grep -q '^variable "msft_tenant_id"' variables.tf
+    if [[ $? -eq 0 ]]; then
+      printf "# Azure AD Apps (Microsoft API Clients) will be provisioned in the following tenant to access your Microsoft 365 data\n" >> $TFVARS_FILE
+      printf "#  - this should be the ID of your Microsoft 365 organization (tenant)\n" >> $TFVARS_FILE
+      printf "#  - if you're not connecting to Microsoft 365 data sources, you can omit this value\n" >> $TFVARS_FILE
+      MSFT_TENANT_ID=$(az account show --query tenantId --output tsv)
+      printf "msft_tenant_id=\"${MSFT_TENANT_ID}\"\n\n" >> $TFVARS_FILE
+      printf "\tmsft_tenant_id=${BLUE}\"${MSFT_TENANT_ID}\"${NC}\n"
 
-    MSFT_USER_EMAIL=$(az account show --query user.name --output tsv)
-    printf "# users in the following list will be set as the 'owners' of the Azure AD Apps (API clients) provisioned to access your Microsoft 365 data\n" >> $TFVARS_FILE
-    printf "#  - if you're not connecting to Microsoft 365 data sources, you can omit this value\n" >> $TFVARS_FILE
-    printf "msft_owners_email=[\n  \"${MSFT_USER_EMAIL}\"\n]\n\n" >> $TFVARS_FILE
-    printf "\tmsft_owners_email=${BLUE}[ \"${MSFT_USER_EMAIL}\" ]${NC}\n"
+      MSFT_USER_EMAIL=$(az account show --query user.name --output tsv)
+      printf "# users in the following list will be set as the 'owners' of the Azure AD Apps (API clients) provisioned to access your Microsoft 365 data\n" >> $TFVARS_FILE
+      printf "#  - if you're not connecting to Microsoft 365 data sources, you can omit this value\n" >> $TFVARS_FILE
+      printf "msft_owners_email=[\n  \"${MSFT_USER_EMAIL}\"\n]\n\n" >> $TFVARS_FILE
+      printf "\tmsft_owners_email=${BLUE}[ \"${MSFT_USER_EMAIL}\" ]${NC}\n"
+    fi
   else
     printf "${RED}az not available${NC}\n"
   fi
