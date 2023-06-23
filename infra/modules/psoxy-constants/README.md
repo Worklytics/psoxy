6c8caa01be4f66bfd8e6b183e17f369f092b6c37
Erik Schultink <erik@worklytics.co>
2023-06-23 11:08:51 -0700

    psoxy constants module

---
+++ infra/modules/psoxy-constants/README.md
@@ -0,0 +1,4 @@
+# psoxy-constants
+
+Provider-less module that defines a bunch of constants that you might need to provision Psoxy, or
+bootstrap stuff needed for provisioning.
---
+++ infra/modules/psoxy-constants/outputs.tf
@@ -0,0 +1,20 @@
+
+output "required_aws_roles_to_provision_host" {
+  value       = local.required_aws_roles_to_provision_host
+  description = "The AWS roles required to provision infrastructure needed to host Psoxy in AWS"
+}
+
+output "required_gcp_roles_to_provision_host" {
+  value       = local.required_gcp_roles_to_provision_host
+  description = "The GCP roles required to provision infrastructure needed to host Psoxy in GCP"
+}
+
+output "required_gcp_roles_to_provision_google_workspace_source" {
+  value       = local.required_gcp_roles_to_provision_google_workspace_source
+  description = "The GCP roles required to provision OAuth Client(s) needed to use Google Workspace as a data source via Psoxy"
+}
+
+output "required_azuread_roles_to_provision_msft_365_source" {
+  value       = local.required_azuread_roles_to_provision_msft_365_source
+  description = "The Azure AD roles required to provision OAuth Client(s) needed to use Microsoft 365 as a data source via Psoxy"
+}
