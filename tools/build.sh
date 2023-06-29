631e7cb31347b8f0f95ccb613ef2602b98c08c70
Erik Schultink <eschultink@users.noreply.github.com>
2023-06-29 14:39:31 -0700

    release tooling 2 (#397)
    * fix failure to cleanup in gcp case
    
    * fix publish legacy example docs
    
    * rename release tool
    
    * be a little more strict after copy
    
    * push to origin, create the pr
    
    * move all release tooling to own directory
    
    * fix quiet option in build
    
    * add instructions for testing init-example.sh; clarify build script generation part
    
    * fix update-bundle perms, quiet build
    
    * add testing instructions
    
    * fix extra n char

--- tools/build.sh
+++ tools/build.sh
@@ -3,9 +3,6 @@
 #see ../infra/modules/psoxy-package/build.sh
 # this is similar, but outputs errors directly instead of to log file; and doesn'
 
-IMPLEMENTATION=$1 # 'aws' or 'gcp'
-JAVA_SOURCE_ROOT=$2
-
 while getopts ":q" opt; do
   case $opt in
     q)
@@ -13,6 +10,9 @@ while getopts ":q" opt; do
       ;;
   esac
 done
+
+IMPLEMENTATION=${@:$OPTIND:1}
+JAVA_SOURCE_ROOT=${@:$OPTIND+1:1}
 
 # set to fail on errors
 set -e
--- tools/update-bundle.sh
+++ tools/update-bundle.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Testing
+# from in example directory, such as `infra/examples/aws-msft-365`:
+#  ../../../tools/update-bundle.sh ~/code/psoxy/ terraform.tfvars aws
+
 PSOXY_BASE_DIR=$1
 TFVARS_FILE=$2
 HOST_PLATFORM=$3
@@ -13,7 +17,7 @@ RELEASE_VERSION=$(sed -n -e 's/.*<revision>\(.*\)<\/revision>.*/\1/p' "${PSOXY_B
 
 printf "Building psoxy bundle from code checkout at ${BLUE}${PSOXY_BASE_DIR}${NC} for ${BLUE}${HOST_PLATFORM}${NC}; this will take a few minutes ...\n"
 
-${PSOXY_BASE_DIR}tools/build.sh ${HOST_PLATFORM} ${PSOXY_BASE_DIR}java -q
+${PSOXY_BASE_DIR}tools/build.sh -q ${HOST_PLATFORM} ${PSOXY_BASE_DIR}java
 
 cp ${PSOXY_BASE_DIR}java/impl/${HOST_PLATFORM}/target/psoxy-${HOST_PLATFORM}-${RELEASE_VERSION}.jar .
 
@@ -28,18 +32,19 @@ fi
 
 if grep -q '^[[:space:]]*deployment_bundle' "$TFVARS_FILE" ; then
   sed -i .bck "/^[[:space:]]*deployment_bundle.*/c\\
-deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"\\n" "$TFVARS_FILE"
-  rm *.bck
+deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"" "$TFVARS_FILE"
+  rm ${TFVARS_FILE}.bck
 else
-  echo "deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"" >> $TFVARS_FILE
+  printf "deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"\n\n" >> $TFVARS_FILE
 fi
 
 printf "Deployment bundle built: ${BLUE}${DEPLOYMENT_BUNDLE}${NC}. You should commit this to your repo.\n"
 
+
 echo "#!/bin/bash" > ./update-bundle
 printf "\n# Use this script rebuild your deployment bundle and update your terraform.tfvars to use the new one\n" >> ./update-bundle
 echo "${0} ${@}" >> ./update-bundle
-chmod +x update-bundle
+chmod +x ./update-bundle
 
 printf "If you update your proxy version in the future, you can use the ${BLUE}update-bundle${NC} script we've generated in your working directory to update it.\n"
 
