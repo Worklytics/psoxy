eb352de0ca32d70ea743cd017b51a235fdff6459
Erik Schultink <erik@worklytics.co>
2023-06-28 22:33:28 -0700

    fix update-bundle perms, quiet build

--- tools/update-bundle.sh
+++ tools/update-bundle.sh
@@ -13,7 +13,7 @@ RELEASE_VERSION=$(sed -n -e 's/.*<revision>\(.*\)<\/revision>.*/\1/p' "${PSOXY_B
 
 printf "Building psoxy bundle from code checkout at ${BLUE}${PSOXY_BASE_DIR}${NC} for ${BLUE}${HOST_PLATFORM}${NC}; this will take a few minutes ...\n"
 
-${PSOXY_BASE_DIR}tools/build.sh ${HOST_PLATFORM} ${PSOXY_BASE_DIR}java -q
+${PSOXY_BASE_DIR}tools/build.sh -q ${HOST_PLATFORM} ${PSOXY_BASE_DIR}java
 
 cp ${PSOXY_BASE_DIR}java/impl/${HOST_PLATFORM}/target/psoxy-${HOST_PLATFORM}-${RELEASE_VERSION}.jar .
 
@@ -29,7 +29,7 @@ fi
 if grep -q '^[[:space:]]*deployment_bundle' "$TFVARS_FILE" ; then
   sed -i .bck "/^[[:space:]]*deployment_bundle.*/c\\
 deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"\\n" "$TFVARS_FILE"
-  rm *.bck
+  rm ${TFVARS_FILE}.bck
 else
   echo "deployment_bundle = \"${DEPLOYMENT_BUNDLE}\"" >> $TFVARS_FILE
 fi
@@ -39,7 +39,7 @@ printf "Deployment bundle built: ${BLUE}${DEPLOYMENT_BUNDLE}${NC}. You should co
 echo "#!/bin/bash" > ./update-bundle
 printf "\n# Use this script rebuild your deployment bundle and update your terraform.tfvars to use the new one\n" >> ./update-bundle
 echo "${0} ${@}" >> ./update-bundle
-chmod +x update-bundle
+chmod +x ./update-bundle
 
 printf "If you update your proxy version in the future, you can use the ${BLUE}update-bundle${NC} script we've generated in your working directory to update it.\n"
 
