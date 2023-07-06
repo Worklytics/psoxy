5fc894c0fd1023c66046ed4a15ef90929194bfd4
Erik Schultink <erik@worklytics.co>
2023-07-06 12:31:44 -0700

    follow resource name convention for function

--- infra/modules/aws-psoxy-lambda/main.tf
+++ infra/modules/aws-psoxy-lambda/main.tf
@@ -42,7 +42,7 @@ locals {
 }
 
 
-resource "aws_lambda_function" "psoxy-instance" {
+resource "aws_lambda_function" "instance" {
   function_name                  = local.function_name
   role                           = aws_iam_role.iam_for_lambda.arn
   architectures                  = ["arm64"] # 20% cheaper per ms exec time than x86_64
@@ -73,6 +73,11 @@ resource "aws_lambda_function" "psoxy-instance" {
       tags
     ]
   }
+}
+
+moved {
+  from = aws_lambda_function.psoxy-instance
+  to   = aws_lambda_function.instance
 }
 
 # cloudwatch group per lambda function
