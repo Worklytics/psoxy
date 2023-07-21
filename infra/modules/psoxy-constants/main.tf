bcddaabc129dfb7cce0bc825f95b49ce878a0d55
Erik Schultink <eschultink@users.noreply.github.com>
2023-07-21 10:14:50 -0700

    more constants in `psoxy-constants` module (#443)
    * aws managed policies for MSFT 365 as source
    
    * consistent nomenclature
    
    * GCP API Services as constants

--- infra/modules/psoxy-constants/main.tf
+++ infra/modules/psoxy-constants/main.tf
@@ -9,8 +9,23 @@ locals {
     "arn:aws:iam::aws:policy/AmazonSSMFullAccess"  = "AmazonSSMFullAccess"
     "arn:aws:iam::aws:policy/AWSLambda_FullAccess" = "AWSLambda_FullAccess"
   }
+  # AWS managed policy required to consume Microsoft 365 data
+  # (in addition to above)
+  required_aws_managed_policies_to_consume_msft_365_source = {
+    "arn:aws:iam::aws:policy/AmazonCognitoPowerUser" = "AmazonCognitoPowerUser"
+  }
+
   # TODO: create IAM policy document, which installer could use to create their own policy as
   # alternative to using AWS Managed policies
+
+  # initial GCP APIs that must be enabled in projects that will host the proxy.
+  # (Terraform apply will enabled additional ones)
+  required_gcp_apis_to_host = {
+    # https://console.cloud.google.com/apis/library/iamcredentials.googleapis.com
+    "iamcredentials.googleapis.com" = "IAM Service Account Credentials API",
+    # https://console.cloud.google.com/apis/library/serviceusage.googleapis.com
+    "serviceusage.googleapis.com"   = "Service Usage API",
+  }
 
   required_gcp_roles_to_provision_host = {
     "roles/storage.admin" = {
@@ -37,7 +52,12 @@ locals {
       display_name    = "Cloud Functions Admin",
       description_url = "https://cloud.google.com/iam/docs/understanding-roles#cloudfunctions.admin"
     },
-  } # TODO: add list of permissions, which customer could use to create custom role as alternative
+  }
+  # TODO: add list of permissions, which customer could use to create custom role as alternative
+
+
+  # TODO: confirm that this is indeed the same list (believe it is)
+  required_gcp_apis_to_provision_google_workspace_source = local.required_gcp_apis_to_host
 
   required_gcp_roles_to_provision_google_workspace_source = {
     "roles/iam.serviceAccountAdmin" = {
