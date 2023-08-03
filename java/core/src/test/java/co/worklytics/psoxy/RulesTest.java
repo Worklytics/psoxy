0c74ace860eb7ed80913d5d4d247d405ea9e0258
Erik Schultink <erik@worklytics.co>
2023-08-03 16:55:05 -0700

    example of simple survey rules that this version is OK with

--- java/core/src/test/java/co/worklytics/psoxy/RulesTest.java
+++ java/core/src/test/java/co/worklytics/psoxy/RulesTest.java
@@ -6,7 +6,11 @@ import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
+import java.util.Base64;
+import java.util.Collection;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -70,6 +74,53 @@ public class RulesTest {
         assertTrue(pattern.matcher("/calendar/v3/calendars/primary/events/1248asdfas4532").matches());
 
         assertEquals(Pattern.compile("/calendar/v3/calendars/.*/events.*").pattern(), pattern.pattern());
+    }
+
+    @SneakyThrows
+    @Test
+    public void noredactions_throws_error() {
+
+        String yamlSurvey =
+            "pseudonymizations:\n" +
+            "  - csvColumns:\n" +
+            "      - \"EMPLOYEE_ID\"\n";
+
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        Rules fromYaml = objectMapper.readerFor(Rules.class)
+            .readValue(yamlSurvey);
+
+        assertThrows(NullPointerException.class,
+            () -> fromYaml.getRedactions().stream());
+
+    }
+
+    @SneakyThrows
+    @Test
+    public void simple_survey() {
+
+        String yamlSurvey =
+            "pseudonymizations:\n" +
+                "  - csvColumns:\n" +
+                "      - \"employee_id\"\n" +
+                "redactions:\n" +
+                "  - csvColumns:\n" +
+                "      - \"employee_email\"\n"; //not expected, but just in case
+
+        Base64.getEncoder().encode(yamlSurvey.getBytes());
+
+
+        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+        Rules fromYaml = objectMapper.readerFor(Rules.class)
+            .readValue(yamlSurvey);
+
+        assertEquals(1, fromYaml.getRedactions().stream().count());
+
+        List<String> redactions = fromYaml.getRedactions().stream().map(Rules.Rule::getCsvColumns).flatMap(Collection::stream).collect(Collectors.toList());
+
+        assertEquals(1, redactions.size());
+
+        assertEquals("cHNldWRvbnltaXphdGlvbnM6CiAgLSBjc3ZDb2x1bW5zOgogICAgICAtICJlbXBsb3llZV9pZCIKcmVkYWN0aW9uczoKICAtIGNzdkNvbHVtbnM6CiAgICAgIC0gImVtcGxveWVlX2VtYWlsIgo=",
+            Base64.getEncoder().encodeToString(yamlSurvey.getBytes()));
     }
 
 }
