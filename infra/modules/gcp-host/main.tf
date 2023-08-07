f6d8f6797f1fe2b16acc8dc51ef50f55841b3b7a
Erik Schultink <erik@worklytics.co>
2023-08-07 15:35:12 -0700

    support overriding memory use for gcp bulk case

--- infra/modules/gcp-host/main.tf
+++ infra/modules/gcp-host/main.tf
@@ -129,7 +129,6 @@ module "api_connector" {
   invoker_sa_emails                     = var.worklytics_sa_emails
   default_labels                        = var.default_labels
   todos_as_local_files                  = var.todos_as_local_files
-  available_memory_mb                   = try(var.custom_bulk_connector_arguments[each.key].available_memory_mb, null)
 
   environment_variables = merge(
     var.general_environment_variables,
@@ -185,6 +184,7 @@ module "bulk_connector" {
   sanitized_bucket_name         = try(each.value.sanitized_bucket_name, null)
   default_labels                = var.default_labels
   todos_as_local_files          = var.todos_as_local_files
+  available_memory_mb           = coalesce(var.custom_bulk_connector_arguments[each.key].available_memory_mb, null)
 
 
   environment_variables = merge(
