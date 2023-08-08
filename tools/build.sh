a8762d3978d638950827173a83282c5bf8fb82b3
Erik Schultink <erik@worklytics.co>
2023-08-08 12:13:47 -0700

    improve error handling / build feedback

--- tools/build.sh
+++ tools/build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 #see ../infra/modules/psoxy-package/build.sh
-# this is similar, but outputs errors directly instead of to log file; and doesn'
+# this is similar, but outputs errors directly instead of to log file
+
+# colors
+RED='\e[0;31m'
+BLUE='\e[0;34m'
+NC='\e[0m' # No Color
 
 while getopts ":q" opt; do
   case $opt in
@@ -13,6 +18,19 @@ done
 
 IMPLEMENTATION=${@:$OPTIND:1}
 JAVA_SOURCE_ROOT=${@:$OPTIND+1:1}
+
+
+if [[ "$IMPLEMENTATION" != "aws" && "$IMPLEMENTATION" != "gcp" ]]; then
+    printf "${RED}Error: HOST_PLATFORM value '${IMPLEMENTATION}' must be 'aws' or 'gcp'.${NC}\n"
+    printf "Usage: build.sh [-q] <IMPLEMENTATION> <JAVA_SOURCE_ROOT>\n"
+    exit 1
+fi
+
+if [[ -z "$JAVA_SOURCE_ROOT" ]]; then
+    printf "${RED}Error: JAVA_SOURCE_ROOT value is required.${NC}\n"
+    printf "Usage: build.sh [-q] <IMPLEMENTATION> <JAVA_SOURCE_ROOT>\n"
+    exit 1
+fi
 
 # set to fail on errors
 set -e
