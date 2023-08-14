e44b826f3246aae9b736735a345942b58701ad91
Erik Schultink <erik@worklytics.co>
2023-08-14 14:12:47 -0700

    caching in GCP auth strategy

--- java/core/src/main/java/co/worklytics/psoxy/gateway/impl/GoogleCloudPlatformServiceAccountKeyAuthStrategy.java
+++ java/core/src/main/java/co/worklytics/psoxy/gateway/impl/GoogleCloudPlatformServiceAccountKeyAuthStrategy.java
@@ -10,8 +10,13 @@ import com.google.auth.http.HttpTransportFactory;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.common.annotations.VisibleForTesting;
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import lombok.extern.java.Log;
 import org.apache.commons.lang3.StringUtils;
@@ -19,6 +24,7 @@ import org.apache.commons.lang3.StringUtils;
 import javax.inject.Inject;
 import java.io.ByteArrayInputStream;
 import java.util.*;
+import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
 
 
@@ -43,49 +49,46 @@ public class GoogleCloudPlatformServiceAccountKeyAuthStrategy implements SourceA
     @Inject ConfigService config;
     @Inject HttpTransportFactory httpTransportFactory;
 
+
+    transient Set<String> scopes;
+    transient GoogleCredentials credentials;
+    transient LoadingCache<String, GoogleCredentials> credentialsCache = CacheBuilder.newBuilder()
+        .concurrencyLevel(1)
+        .maximumSize(50) // as we usually shard per Google Workspace user account, this should be ~ number of active shards
+        .recordStats()
+        .build(CacheLoader.from(this::buildImpersonatedCredentials));
+
+
     @SneakyThrows
     @Override
     public Credentials getCredentials(Optional<String> userToImpersonate) {
-
-        Set<String> scopes = Arrays.stream(config.getConfigPropertyOrError(ConfigProperty.OAUTH_SCOPES).split(" "))
-            .collect(Collectors.toSet());
-        GoogleCredentials credentials;
-
-        Optional<String> key = config.getConfigPropertyAsOptional(ConfigProperty.SERVICE_ACCOUNT_KEY);
-
-        if (key.isPresent()) {
-            ByteArrayInputStream boas = key.map(this::toStream).orElseThrow();
-            credentials = ServiceAccountCredentials.fromStream(boas, httpTransportFactory);
+        if (userToImpersonate.isEmpty()) {
+            return this.getBaseCredentials();
         } else {
-            credentials = GoogleCredentials.getApplicationDefault();
-        }
-
-        if (userToImpersonate.isPresent()) {
-            if (!(credentials instanceof ServiceAccountCredentials)) {
-                // only ServiceAccountCredentials (created from an actual service account key) support
-                // domain-wide delegation
-                // see examples - even when access is 'global', still need to impersonate a user
-                // https://developers.google.com/admin-sdk/reports/v1/guides/delegation
-                log.warning("Trying to impersonate user with credentials that don't support it");
+            try {
+                return credentialsCache.get(userToImpersonate.get());
+            } catch (ExecutionException e) {  // can't do whole thing in one-line bc of checked exception
+                throw e.getCause();
             }
-
-            //even though GoogleCredentials implements `createDelegated`, it's a no-op if the
-            // credential type doesn't support it.
-            credentials = credentials.createDelegated(userToImpersonate.get());
         }
-
-        credentials = credentials.createScoped(scopes);
-
-        return credentials;
     }
 
     @VisibleForTesting
-    ByteArrayInputStream toStream(String base64encodedKey) {
-        return new ByteArrayInputStream(Base64.getDecoder().decode(
-            //strip whitespace around base64-encoded string; have seen these with artifacts from
-            // copy-paste of the SA key into cloud consoles
-            StringUtils.strip(base64encodedKey))
-        );
+    GoogleCredentials buildImpersonatedCredentials(@NonNull String accountToImpersonate) {
+
+        if (!(getBaseCredentials() instanceof ServiceAccountCredentials)) {
+            // only ServiceAccountCredentials (created from an actual service account key) support
+            // domain-wide delegation
+            // see examples - even when access is 'global', still need to impersonate a user
+            // https://developers.google.com/admin-sdk/reports/v1/guides/delegation
+            log.warning("Trying to impersonate user with credentials that don't support it");
+        }
+
+        //even though GoogleCredentials implements `createDelegated`, it's a no-op if the
+        // credential type doesn't support it.
+        GoogleCredentials impersonated = getBaseCredentials().createDelegated(accountToImpersonate);
+        impersonated = impersonated.createScoped(getScopes());
+        return impersonated;
     }
 
     @Override
@@ -101,5 +104,38 @@ public class GoogleCloudPlatformServiceAccountKeyAuthStrategy implements SourceA
         return Set.of(ConfigProperty.values());
     }
 
+    @SneakyThrows
+    synchronized GoogleCredentials getBaseCredentials() {
+        if (credentials == null) {
+            Optional<String> key = config.getConfigPropertyAsOptional(ConfigProperty.SERVICE_ACCOUNT_KEY);
+
+            GoogleCredentials provisional;
+            if (key.isPresent()) {
+                ByteArrayInputStream boas = key.map(this::toStream).orElseThrow();
+                provisional = ServiceAccountCredentials.fromStream(boas, httpTransportFactory);
+            } else {
+                provisional = GoogleCredentials.getApplicationDefault();
+            }
+            credentials = provisional.createScoped(getScopes());
+        }
+        return credentials;
+    }
+
+    synchronized Set<String> getScopes() {
+        if (scopes == null) {
+            scopes = Arrays.stream(config.getConfigPropertyOrError(ConfigProperty.OAUTH_SCOPES).split(" "))
+                .collect(Collectors.toSet());
+        }
+        return scopes;
+    }
+
+    @VisibleForTesting
+    ByteArrayInputStream toStream(String base64encodedKey) {
+        return new ByteArrayInputStream(Base64.getDecoder().decode(
+            //strip whitespace around base64-encoded string; have seen these with artifacts from
+            // copy-paste of the SA key into cloud consoles
+            StringUtils.strip(base64encodedKey))
+        );
+    }
 
 }
