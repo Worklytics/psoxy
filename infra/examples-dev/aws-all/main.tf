e8697231377562298ece9440073bec5a5f5a62de
Erik Schultink <erik@worklytics.co>
2023-09-13 14:00:14 -0700

    split backend out of main.tf

--- infra/examples-dev/aws-all/main.tf
+++ infra/examples-dev/aws-all/main.tf
@@ -7,22 +7,9 @@ terraform {
     }
   }
 
-  # we recommend you use a secure location for your Terraform state (such as S3 bucket), as it
-  # may contain sensitive values (such as API keys) depending on which data sources you configure.
-  #
-  # local may be safe for production-use IFF you are executing Terraform from a secure location
-  #
-  # Please review and seek guidance from your Security team if in doubt.
-  backend "local" {
-  }
-
-  # example remove backend (this S3 bucket must already be provisioned, and AWS role executing
-  # terraform must be able to read/write to it - and use encryption key, if any)
-  #  backend "s3" {
-  #    bucket = "terraform_state_bucket" # fill with S3 bucket where you want the statefile to be
-  #    key    = "prod_state" # fill with path where you want state file to be stored
-  #    region = "us-east-1" # cannot be a variable
-  #  }
+  # NOTE: Terraform backend block is configured in a separate 'backend.tf' file, as expect everyone
+  # to customize it for their own use; whereas `main.tf` should be largely consistent across
+  # deployments
 }
 
 
---
+++ infra/examples-dev/gcp/backend.tf
@@ -0,0 +1,18 @@
+terraform {
+  # we recommend you use a secure location for your Terraform state (such as S3 bucket), as it
+  # may contain sensitive values (such as API keys) depending on which data sources you configure.
+  #
+  # local may be safe for production-use IFF you are executing Terraform from a secure location
+  #
+  # Please review and seek guidance from your Security team if in doubt.
+  backend "local" {
+  }
+
+  # example remote backend (this GCS bucket must already be provisioned, and GCP user executing
+  # terraform must be able to read/write to it)
+  #  backend "gcs" {
+  #    bucket  = "tf-state-prod"
+  #    prefix  = "proxy/terraform-state"
+  #  }
+
+}
