b475dcb7294241a743bbfe20472cfffcc5d33ca7
Erik Schultink <erik@worklytics.co>
2023-10-17 15:26:22 -0700

    better tests

--- java/gateway-core/src/main/java/com/avaulta/gateway/rules/BulkDataRules.java
+++ java/gateway-core/src/main/java/com/avaulta/gateway/rules/BulkDataRules.java
@@ -1,5 +1,11 @@
 package com.avaulta.gateway.rules;
 
+import com.avaulta.gateway.rules.transforms.EncryptIp;
+import com.avaulta.gateway.rules.transforms.HashIp;
+import com.avaulta.gateway.rules.transforms.Transform;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
 /**
  * rules that define how to sanitize bulk (file) data
  *
@@ -7,6 +13,11 @@ package com.avaulta.gateway.rules;
  *
  *
  */
+@JsonTypeInfo(use = JsonTypeInfo.Id.DEDUCTION)
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = RecordRules.class, name = "record"),
+    @JsonSubTypes.Type(value = ColumnarRules.class, name = "columnar"),
+})
 public interface BulkDataRules extends RuleSet {
 
 }
--- java/gateway-core/src/test/java/com/avaulta/gateway/rules/RecordRulesTest.java
+++ java/gateway-core/src/test/java/com/avaulta/gateway/rules/RecordRulesTest.java
@@ -54,4 +54,5 @@ public class RecordRulesTest {
 
         assertEquals(EXPECTED, yamlMapper.writeValueAsString(recordRules));
     }
+
 }
