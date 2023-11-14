b481e2582e92fa4aa384cd155152d2836d110db2
Erik Schultink <erik@worklytics.co>
2023-11-14 15:00:41 -0800

    health check logs less verbose, as otherwise confuses people

--- java/core/src/main/java/co/worklytics/psoxy/gateway/impl/HealthCheckRequestHandler.java
+++ java/core/src/main/java/co/worklytics/psoxy/gateway/impl/HealthCheckRequestHandler.java
@@ -30,8 +30,10 @@ import java.util.stream.Collectors;
 @Log
 public class HealthCheckRequestHandler {
 
-    final String JAVA_SOURCE_CODE_VERSION = "rc-v0.4.41";
+    static final String JAVA_SOURCE_CODE_VERSION = "rc-v0.4.41";
 
+    @Inject
+    EnvVarsConfigService envVarsConfigService;
     @Inject
     ConfigService config;
     @Inject
@@ -52,6 +54,12 @@ public class HealthCheckRequestHandler {
         return request.getHeader(ControlHeader.HEALTH_CHECK.getHttpHeader()).isPresent();
     }
 
+    private void logInDev(String message, Throwable e) {
+        if (envVarsConfigService.isDevelopment()) {
+            log.log(Level.WARNING, message, e);
+        }
+    }
+
     private HttpEventResponse handle() {
         Set<String> missing =
                 sourceAuthStrategy.getRequiredConfigProperties().stream()
@@ -66,7 +74,7 @@ public class HealthCheckRequestHandler {
                 missing.add(ProxyConfigProperty.TARGET_HOST.name());
             }
         } catch (Throwable ignored) {
-            log.log(Level.WARNING, "Failed to add TARGET_HOST info to health check", ignored);
+            logInDev("Failed to add TARGET_HOST info to health check", ignored);
         }
 
         HealthCheckResult.HealthCheckResultBuilder healthCheckResult = HealthCheckResult.builder()
@@ -82,7 +90,7 @@ public class HealthCheckRequestHandler {
                 .map(Boolean::parseBoolean)
                 .ifPresent(healthCheckResult::pseudonymizeAppIds);
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add pseudonymizeAppIds to health check", e);
+            logInDev("Failed to add pseudonymizeAppIds to health check", e);
         }
 
         try {
@@ -93,35 +101,35 @@ public class HealthCheckRequestHandler {
                                     .map(metadata -> metadata.getLastModifiedDate().orElse(null))
                                     .orElse(null))));
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add config debug info to health check", e);
+            logInDev("Failed to add config debug info to health check", e);
         }
 
         try {
             config.getConfigPropertyAsOptional(ProxyConfigProperty.SOURCE_AUTH_STRATEGY_IDENTIFIER)
                     .ifPresent(healthCheckResult::sourceAuthStrategy);
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add sourceAuthStrategy to health check", e);
+            logInDev("Failed to add sourceAuthStrategy to health check", e);
         }
 
         try {
             config.getConfigPropertyAsOptional(OAuthRefreshTokenSourceAuthStrategy.ConfigProperty.GRANT_TYPE)
                     .ifPresent(healthCheckResult::sourceAuthGrantType);
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add sourceAuthGrantType to health check", e);
+            logInDev("Failed to add sourceAuthGrantType to health check", e);
         }
 
         try {
             config.getConfigPropertyAsOptional(ProxyConfigProperty.BUNDLE_FILENAME)
                     .ifPresent(healthCheckResult::bundleFilename);
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add bundleFilename to health check", e);
+            logInDev("Failed to add bundleFilename to health check", e);
         }
 
         try {
             rulesUtils.getRulesFromConfig(config)
                     .ifPresent(rules -> healthCheckResult.rules(rulesUtils.asYaml(rules)));
         } catch (Throwable e) {
-            log.log(Level.WARNING, "Failed to add rules to health check", e);
+            logInDev("Failed to add rules to health check", e);
         }
 
         HttpEventResponse.HttpEventResponseBuilder responseBuilder = HttpEventResponse.builder();
