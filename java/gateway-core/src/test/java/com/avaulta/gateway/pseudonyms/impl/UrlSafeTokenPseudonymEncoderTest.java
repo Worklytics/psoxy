e3e10b0900535e99a9cb2e41889f60d75b55207f
Erik Schultink <erik@worklytics.co>
2023-12-04 10:00:35 -0800

    add some tests/checks, to prove out a case

--- java/gateway-core/src/test/java/com/avaulta/gateway/pseudonyms/impl/UrlSafeTokenPseudonymEncoderTest.java
+++ java/gateway-core/src/test/java/com/avaulta/gateway/pseudonyms/impl/UrlSafeTokenPseudonymEncoderTest.java
@@ -14,8 +14,7 @@ import org.junit.jupiter.params.provider.ValueSource;
 import java.util.Base64;
 import java.util.function.Function;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UrlSafeTokenPseudonymEncoderTest {
 
@@ -143,6 +142,21 @@ public class UrlSafeTokenPseudonymEncoderTest {
             forComparison.encodeToString(pseudonymEncoder.decode(pseudonymEncoder.encode(p)).getHash()),
             forComparison.encodeToString(pseudonymEncoder.decode(pseudonymEncoder.encode(r)).getHash())
         );
+
+    }
+
+    /**
+     * test to reproduce exception we've seen in logs, just to establish what scenario leads to it
+     */
+    @Test
+    void decoder2eException() {
+        try {
+            pseudonymEncoder.decode("t~UFdK0TvVTvZ23c6QslyCy0o2MSq2DRtDjEXfTPJyyMkacme.com");
+            fail("expected exception");
+        } catch (IllegalArgumentException e) {
+            // 2e is the . in the domain, which wasn't stripped off bc missing '@'
+            assertEquals("Illegal base64 character 2e", e.getMessage());
+        }
 
     }
 }
