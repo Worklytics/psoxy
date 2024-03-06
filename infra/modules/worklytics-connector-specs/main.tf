2ec25cf9d5eda2bf840bb48f873de1dfac8041d2
Andrés Pérez <andres@worklytics.co>
2024-03-06 17:08:23 +0100

    Updated doc in spec

--- infra/modules/worklytics-connector-specs/main.tf
+++ infra/modules/worklytics-connector-specs/main.tf
@@ -952,6 +952,7 @@ EOT
 Zoom connector through Psoxy requires a custom managed app on the Zoom Marketplace (in development
 mode, no need to publish).
 1. Go to https://marketplace.zoom.us/develop/create and create an app of type "Server to Server OAuth"
+   for creating a server-to-server app.
 2. After creation it will show the App Credentials. Share them with the AWS/GCP administrator, the
 following secret values must be filled in the Secret Manager for the Proxy with the appropriate values:
 - `PSOXY_ZOOM_CLIENT_ID`
@@ -959,13 +960,14 @@ following secret values must be filled in the Secret Manager for the Proxy with 
 - `PSOXY_ZOOM_CLIENT_SECRET`
 Anytime the *client secret* is regenerated it needs to be updated in the Proxy too.
 3. Fill the information section
-4. Fill the scopes section, enabling the following:
-- Users / View all user information /user:read:admin
-  - To be able to gather information about the zoom users
-- Meetings / View all user meetings /meeting:read:admin
-  - Allows us to list all user meeting
-- Report / View report data /report:read:admin
-  - Last 6 months view for user meetings
+4. Fill the scopes section clicking on `+ Add Scopes` and adding the following:
+- User:
+  - View all user information: `user:read:admin`. This is to be able to gather information about the zoom users.
+- Meeting:
+  - View all user meetings: `meeting:read:admin`. That allows us to list all user meeting
+- Report:
+  - View report data: `report:read:admin`. This is for getting last 6 months view for user meetings
+Once the scopes are added, click on `Done` and then `Continue`.
 5. Activate the app
 EOT
     },
