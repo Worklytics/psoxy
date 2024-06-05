12ffd901ad0927af9ec1499e39c27e94d52964c7
Andrés Pérez <andres@worklytics.co>
2024-06-05 14:38:56 +0200

    Using data

--- infra/modules/azuread-connection/main.tf
+++ infra/modules/azuread-connection/main.tf
@@ -12,9 +12,8 @@ terraform {
 
 data "azuread_application_published_app_ids" "well_known" {}
 
-resource "azuread_service_principal" "msgraph" {
+data "azuread_service_principal" "msgraph" {
   application_id = data.azuread_application_published_app_ids.well_known.result.MicrosoftGraph
-  use_existing   = true
 }
 
 resource "azuread_application" "connector" {
@@ -40,7 +39,7 @@ resource "azuread_application" "connector" {
       content {
         # this approach is consistent with what you get via `az ad sp list`, which is what MSFT docs
         # recommend: https://docs.microsoft.com/en-us/graph/permissions-reference#retrieving-permission-ids
-        id   = azuread_service_principal.msgraph.oauth2_permission_scope_ids[resource_access.value]
+        id   = data.azuread_service_principal.msgraph.oauth2_permission_scope_ids[resource_access.value]
         type = "Scope"
       }
     }
@@ -49,7 +48,7 @@ resource "azuread_application" "connector" {
       for_each = var.required_app_roles
 
       content {
-        id   = azuread_service_principal.msgraph.app_role_ids[resource_access.value]
+        id   = data.azuread_service_principal.msgraph.app_role_ids[resource_access.value]
         type = "Role"
       }
     }
