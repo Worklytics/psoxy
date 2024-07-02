b5e98e9955864d918ae23f0aacba2658a7d7cd31
Andrés Pérez <andres@worklytics.co>
2024-07-02 14:28:41 +0200

    Lint

--- infra/modules/worklytics-connectors-msft-365/main.tf
+++ infra/modules/worklytics-connectors-msft-365/main.tf
@@ -24,7 +24,7 @@ module "worklytics_connector_specs" {
 locals {
   todos_to_populate = { for k, v in module.worklytics_connector_specs.enabled_msft_365_connectors : k => v if try(v.external_token_todo != null, false) && var.todos_as_local_files }
   application_ids_for_teams_setup = join(",", compact([format("\"%s\"", module.msft_connection["msft-teams"].connector.application_id),
-    try(format("\"%s\"", module.msft_connection["outlook-cal"].connector.application_id), null)]))
+  try(format("\"%s\"", module.msft_connection["outlook-cal"].connector.application_id), null)]))
 }
 
 data "azuread_client_config" "current" {
@@ -70,10 +70,10 @@ resource "local_file" "todo-with-external-todo" {
   for_each = local.todos_to_populate
 
   filename = module.msft_365_grants[each.key].filename
-  content  = format("%s\n## Setup\nThen, please follow next instructions for complete the setup: \n\n%s",
+  content = format("%s\n## Setup\nThen, please follow next instructions for complete the setup: \n\n%s",
     module.msft_365_grants[each.key].todo,
     replace(each.value.external_token_todo, "%%entraid.application_ids%%",
-      each.key == "msft-teams" ? local.application_ids_for_teams_setup : format("\"%s\"", module.msft_connection[each.key].connector.application_id)))
+  each.key == "msft-teams" ? local.application_ids_for_teams_setup : format("\"%s\"", module.msft_connection[each.key].connector.application_id)))
 }
 
 locals {
