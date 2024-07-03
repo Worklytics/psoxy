39d1923fba3e42a6988516541f2689cd429d3bac
Erik Schultink <eschultink@users.noreply.github.com>
2024-07-03 10:47:04 -0700

    BYO msft connector app (#741)
    * msft specs structure (schema) consistent
    
    * allow msft connector creation to be skipped
    
    * scaffolding to use a msft connector app that's provisioned OUTSIDE of terraform
    
    * fix up msft-365 logic
    
    * cleanup application_id references; fix various bugs
    
    * TODO file for shared msft case
    
    * fix TODOs as local files in aws case
    
    * one more missing TODO var

--- infra/examples-dev/aws-all/msft-365.tf
+++ infra/examples-dev/aws-all/msft-365.tf
@@ -4,7 +4,6 @@ module "worklytics_connectors_msft_365" {
   source = "../../modules/worklytics-connectors-msft-365"
   # source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-msft-365?ref=v0.4.56"
 
-
   enabled_connectors                  = var.enabled_connectors
   environment_id                      = var.environment_name
   msft_tenant_id                      = var.msft_tenant_id
@@ -15,6 +14,7 @@ module "worklytics_connectors_msft_365" {
   msft_teams_example_chat_guid        = var.msft_teams_example_chat_guid
   msft_teams_example_call_guid        = var.msft_teams_example_call_guid
   msft_teams_example_call_record_guid = var.msft_teams_example_call_record_guid
+  msft_connector_app_object_id        = var.msft_connector_app_object_id
   todos_as_local_files                = var.todos_as_local_files
   todo_step                           = 1
 }
@@ -53,6 +53,17 @@ module "cognito_identity_pool" {
   name                    = "${local.env_qualifier}-azure-ad-federation"
 }
 
+locals {
+  # either ONE shared, or ONE per connector
+  shared_connector = var.msft_connector_app_object_id == null ? null : module.worklytics_connectors_msft_365.enabled_api_connectors[keys(module.worklytics_connectors_msft_365.enabled_api_connectors)[0]]
+  cognito_identity_login_ids = var.msft_connector_app_object_id == null ? {
+      for k, msft_connector in module.worklytics_connectors_msft_365.enabled_api_connectors :
+      k => msft_connector.connector.client_id
+    } : {
+      "shared" : local.shared_connector.connector.client_id
+    }
+}
+
 module "cognito_identity" {
   count = local.msft_365_enabled ? 1 : 0 # only provision identity pool if MSFT-365 connectors are enabled
 
@@ -63,8 +74,8 @@ module "cognito_identity" {
   aws_role         = var.aws_assume_role_arn
   identity_pool_id = module.cognito_identity_pool[0].pool_id
   login_ids = {
-    for k, v in module.worklytics_connectors_msft_365.enabled_api_connectors :
-    k => "${local.developer_provider_name}=${v.connector.application_id}"
+    for k, client_id in local.cognito_identity_login_ids :
+     k => "${local.developer_provider_name}=${client_id}"
   }
 }
 
@@ -75,8 +86,18 @@ resource "aws_iam_role_policy_attachment" "cognito_lambda_policy" {
   policy_arn = module.cognito_identity_pool[0].policy_arn
 }
 
+locals {
+  provision_entraid_apps = var.msft_connector_app_object_id == null
+
+  azuread_federated_credentials_to_provision = local.provision_entraid_apps ? module.worklytics_connectors_msft_365.enabled_api_connectors : {
+      "shared" : merge(
+        local.shared_connector,
+        { display_name: "Shared" })
+    }
+}
+
 module "msft_connection_auth_federation" {
-  for_each = module.worklytics_connectors_msft_365.enabled_api_connectors
+  for_each = local.azuread_federated_credentials_to_provision
 
   source = "../../modules/azuread-federated-credentials"
   # source = "git::https://github.com/worklytics/psoxy//infra/modules/azuread-federated-credentials?ref=v0.4.56"
@@ -96,7 +117,7 @@ locals {
       environment_variables = merge(try(msft_connector.environment_variables, {}),
         {
           IDENTITY_POOL_ID  = module.cognito_identity_pool[0].pool_id,
-          IDENTITY_ID       = module.cognito_identity[0].identity_id[k],
+          IDENTITY_ID       = try(module.cognito_identity[0].identity_id[k], module.cognito_identity[0].identity_id["shared"]),
           DEVELOPER_NAME_ID = local.developer_provider_name
         }
       )
