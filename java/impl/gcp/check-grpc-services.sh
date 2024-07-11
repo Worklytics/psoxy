5bf055c73adc8468202f5a99f8e80b7a8b53c5b9
Erik Schultink <eschultink@users.noreply.github.com>
2024-07-11 10:52:05 -0700

    prep release v0.4.57 (#752)
    * update release refs to v0.4.57
    
    * improve CHANGELOG
    
    * port correct building to AWS case; improve check tool

--- java/impl/gcp/check-grpc-services.sh
+++ java/impl/gcp/check-grpc-services.sh
@@ -3,15 +3,23 @@
 # helper script to quickly check the gRPC services in a jar file, which seems to be root cause
 # of the gRPC issue in the cloud function
 
-# determine the jar file to check
-VERSION=$1
-JAR_FILE=target/psoxy-gcp-${VERSION}.jar
-
+RED='\e[0;31m'
 BLUE='\e[34m'
 NC='\e[0m' # No Color
 
+# determine the jar file to check
+VERSION=$1
+if [ -z "$VERSION" ]; then
+  printf "${RED}ERROR: version is required${NC}\n"
+  echo "Usage: $0 <version>"
+  exit 1
+fi
+
+JAR_FILE=target/psoxy-gcp-${VERSION}.jar
+
+
 printf "Checking grpc load balancer services in jar file: ${BLUE}${JAR_FILE}${NC}\n"
-printf " (as of 2024-07-10, good one contains 3 files, including 'io.grpc.internal.PickFirstLoadBalancerProvider')\n"
+printf " (this should contain long list, including 'io.grpc.internal.PickFirstLoadBalancerProvider')\n"
 jar xf $JAR_FILE META-INF/services/io.grpc.LoadBalancerProvider
 
 cat META-INF/services/io.grpc.LoadBalancerProvider
