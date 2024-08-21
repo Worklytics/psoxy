f0320694feeae8e73e3eb10b92b68f0b23aeceb3
Erik Schultink <eschultink@users.noreply.github.com>
2024-08-21 09:46:14 -0700

    update release refs to v0.4.59

--- java/core/src/main/java/co/worklytics/psoxy/gateway/impl/HealthCheckRequestHandler.java
+++ java/core/src/main/java/co/worklytics/psoxy/gateway/impl/HealthCheckRequestHandler.java
@@ -31,7 +31,7 @@ import java.util.stream.Collectors;
 @Log
 public class HealthCheckRequestHandler {
 
-    static final String JAVA_SOURCE_CODE_VERSION = "v0.4.58";
+    static final String JAVA_SOURCE_CODE_VERSION = "v0.4.59";
 
     @Inject
     EnvVarsConfigService envVarsConfigService;
