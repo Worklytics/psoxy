7679b07f36a1fab0d40e7778d4e9078db17d4e71
Erik Schultink <eschultink@users.noreply.github.com>
2025-05-15 10:56:08 -0700

    fix ssm

--- infra/modules/private-key-aws-parameter-user-managed/variables.tf
+++ infra/modules/private-key-aws-parameter-user-managed/variables.tf
@@ -18,3 +18,8 @@ variable "kms_key_id" {
   description = "KMS key ID or ARN to use for encrypting secrets. If not provided, secrets will be encrypted by SSM with its keys (controlled by AWS)."
   default     = null
 }
+
+variable "ssm_path" {
+  type        = string
+  description = "path/prefix in SSM Parameter Store at which to create parameters to hold keys"
+}
