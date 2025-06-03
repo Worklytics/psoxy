3e3d0ba2a507ed25e8158ea9838c1d3af7cc7e11
andres <andres@worklytics.co>
2025-06-03 14:37:19 +0200

    Added example calls

--- infra/modules/worklytics-connector-specs/main.tf
+++ infra/modules/worklytics-connector-specs/main.tf
@@ -714,6 +714,8 @@ EOT
       example_api_calls : [
         "/v2/users",
         "/v2/users/{USER_ID}/meetings",
+        "/v2/users/{USER_ID}/settings",
+        "/v2/users/{USER_ID}/recordings",
         "/v2/meetings/{MEETING_ID}",
         "/v2/meetings/{MEETING_ID}/meeting_summary",
         "/v2/past_meetings/{MEETING_ID}",
