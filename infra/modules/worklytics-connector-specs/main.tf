5796ecefbadeb90ffc34bbb1d11a9bc6cb0a2f3d
Erik Schultink <eschultink@users.noreply.github.com>
2025-06-21 12:00:52 -0400

    cleanup slack instructions

--- infra/modules/worklytics-connector-specs/main.tf
+++ infra/modules/worklytics-connector-specs/main.tf
@@ -595,7 +595,7 @@ EOT
       enable_by_default : true
       worklytics_connector_id : "slack-discovery-api-psoxy",
       worklytics_connector_name : "Slack via Psoxy",
-      display_name : "Slack Discovery API"
+      display_name : "Slack via Discovery API"
       target_host : "www.slack.com"
       source_auth_strategy : "oauth2_access_token"
       oauth_scopes_needed : [
@@ -621,47 +621,7 @@ EOT
         "/api/discovery.conversations.history?reactions=1&team={WORKSPACE_ID}&channel={CHANNEL_ID}&limit=10",
         "/api/discovery.users.list?limit=5",
       ]
-      external_token_todo : <<EOT
-### Slack Discovery Setup
-
-For enabling Slack Discovery with the Psoxy you must first set up an app on your Slack Enterprise
-instance.
-
-1. Go to https://api.slack.com/apps and create an app.
-   - Select "From scratch", choose a name (for example "Worklytics connector") and a development workspace
-2. Take note of your App ID (listed in "App Credentials"), contact your Slack representative and ask
-   them to enable `discovery:read` scope for that App ID.
-   If they also enable `discovery:write` then delete it for safety, the app just needs read access.
-
-The next step depends on your installation approach you might need to change slightly
-
-#### Org wide install
-
-Use this step if you want to install in the whole org, across multiple workspaces.
-
-1. Add a bot scope (not really used, but Slack doesn't allow org-wide installations without a bot scope).
-   The app won't use it at all. Just add for example the `users:read` scope, read-only.
-2. Under "Settings > Manage Distribution > Enable Org-Wide App installation",
-   click on "Opt into Org Level Apps", agree and continue. This allows to distribute the app internally
-   on your organization, to be clear it has nothing to do with public distribution or Slack app directory.
-3. Generate the following URL replacing the placeholder for *YOUR_CLIENT_ID* and save it for
-   later:
-4. Go to "OAuth & Permissions" and add the previous URL as "Redirect URLs"
-5. Go to "Settings > Install App", and choose "Install to Organization". A Slack admin should grant
-   the app the permissions and the app will be installed.
-6. Copy the "User OAuth Token" (also listed under "OAuth & Permissions") and store as
-   `PSOXY_SLACK_DISCOVERY_API_ACCESS_TOKEN` in the psoxy's Secret
-   Manager. Otherwise, share the token with the AWS/GCP administrator completing the implementation.
-
-#### Workspace install
-
-Use this steps if you intend to install in just one workspace within your org.
-
-1. Go to "Settings > Install App", click on "Install into *workspace*"
-2. Copy the "User OAuth Token" (also listed under "OAuth & Permissions") and store as
-   `PSOXY_SLACK_DISCOVERY_API_ACCESS_TOKEN` in the psoxy's Secret
-   Manager. Otherwise, share the token with the AWS/GCP administrator completing the implementation.
-EOT
+      external_token_todo : file("docs/slack/discovery-api/instructions.md")
     }
     zoom = {
       source_kind : "zoom"
