7d2f6c1c03ebac78677656e661713fe0e1c65d72
Erik Schultink <eschultink@users.noreply.github.com>
2025-07-07 15:07:41 -0700

    clean some imports; attempt spacing fix in tests

--- java/core/src/main/java/co/worklytics/psoxy/rules/slack/PrebuiltSanitizerRules.java
+++ java/core/src/main/java/co/worklytics/psoxy/rules/slack/PrebuiltSanitizerRules.java
@@ -1,19 +1,10 @@
 package co.worklytics.psoxy.rules.slack;
 
 import co.worklytics.psoxy.rules.RESTRules;
-import com.avaulta.gateway.pseudonyms.PseudonymEncoder;
-import com.avaulta.gateway.rules.Endpoint;
 import co.worklytics.psoxy.rules.Rules2;
-import com.avaulta.gateway.rules.JsonSchemaFilter;
-import com.avaulta.gateway.rules.transforms.Transform;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.collect.ImmutableMap;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.util.Map;
 
 /**
