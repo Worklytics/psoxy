d8a2a0c873c917ef8c1a3ed2dcd8956cd4641f05
Erik Schultink <eschultink@users.noreply.github.com>
2025-07-09 10:17:48 -0700

    test case to ensure we don't blow up on anon column in the middle

--- java/core/src/test/java/co/worklytics/psoxy/storage/impl/BulkDataSanitizerImplTest.java
+++ java/core/src/test/java/co/worklytics/psoxy/storage/impl/BulkDataSanitizerImplTest.java
@@ -277,6 +277,39 @@ public class BulkDataSanitizerImplTest {
         }
     }
 
+    /**
+     * test case for CSV that includes a column without a name (eg, a column with empty/blank string in the header)
+     *
+     * such cases are not strictly invalid CSV, so we will not blow up; but we will generally fail if anyone really attempts to do anything with them.
+     * so the only supported use case is to just preserve the column as-is, and not apply any rules to it.
+     */
+    @Test
+    @SneakyThrows
+    void defaultRules_anonColumn() {
+        final String TEST_EXAMPLE_FILE = "/csv/hris-default-rules_anon-column.csv";
+        final String EXPECTED = "EMPLOYEE_ID,,employee_EMAIL,MANAGER_id,Manager_Email,JOIN_DATE,ROLE\n" +
+            "\"{\"\"hash\"\":\"\"0zPKqEd-CtbCLB1ZSwX6Zo7uAWUvkpfHGzv9-cuYwZc\"\"}\",anon1,\"{\"\"domain\"\":\"\"worklytics.co\"\",\"\"hash\"\":\"\"Qf4dLJ4jfqZLn9ef4VirvYjvOnRaVI5tf5oLnM65YOA\"\"}\",\"{\"\"hash\"\":\"\"-hN_i1M1DeMAicDVp6LhFgW9lH7r3_LbOpTlXYWpXVI\"\"}\",\"{\"\"domain\"\":\"\"worklytics.co\"\",\"\"hash\"\":\"\"TtDWXFAQxNE8O2w7DuMtEKzTSZXERuUVLCjmd9r6KQ4\"\"}\",2021-01-01,Accounting Manager\n" +
+            "\"{\"\"hash\"\":\"\"-hN_i1M1DeMAicDVp6LhFgW9lH7r3_LbOpTlXYWpXVI\"\"}\",anon2,\"{\"\"domain\"\":\"\"worklytics.co\"\",\"\"hash\"\":\"\"TtDWXFAQxNE8O2w7DuMtEKzTSZXERuUVLCjmd9r6KQ4\"\"}\",,,2020-01-01,CEO\n";
+
+        //padded case (eg, 001 instead of 1 for employee_id), should result in different hashes
+        assertNotEquals(EXPECTED, EXPECTED_HRIS_DEFAULT_RULES);
+
+        ConfigService config = MockModules.provideMock(ConfigService.class);
+        when(config.getConfigPropertyAsOptional(eq(ProxyConfigProperty.RULES)))
+            .thenReturn(Optional.of(Base64.encodeBase64String(TestUtils.getData("sources/hris/csv.yaml"))));
+
+        ColumnarRules rules = (ColumnarRules) rulesUtils.getRulesFromConfig(config, new EnvVarsConfigService()).orElseThrow();
+
+        File inputFile = new File(getClass().getResource(TEST_EXAMPLE_FILE).getFile());
+        columnarFileSanitizerImpl.setRules(rules);
+
+        try (FileReader in = new FileReader(inputFile);
+             StringWriter out = new StringWriter()) {
+            columnarFileSanitizerImpl.sanitize(in, out, pseudonymizer);
+            assertEquals(EXPECTED, out.toString());
+        }
+    }
+
 
     @Test
     @SneakyThrows
