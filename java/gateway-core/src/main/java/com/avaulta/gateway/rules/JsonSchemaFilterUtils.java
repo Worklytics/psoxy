a3e5be26eeb257b5d700490a810a09f3a73b3b67
Erik Schultink <eschultink@users.noreply.github.com>
2025-07-23 12:58:19 -0700

    Update java/gateway-core/src/main/java/com/avaulta/gateway/rules/JsonSchemaFilterUtils.java
    Co-authored-by: aperez-worklytics <75276364+aperez-worklytics@users.noreply.github.com>

--- java/gateway-core/src/main/java/com/avaulta/gateway/rules/JsonSchemaFilterUtils.java
+++ java/gateway-core/src/main/java/com/avaulta/gateway/rules/JsonSchemaFilterUtils.java
@@ -134,7 +134,7 @@ public class JsonSchemaFilterUtils {
         } else if (schema.hasAnyOf()) {
             // Get first schema with matches its inner condition.
             // See https://json-schema.org/understanding-json-schema/reference/combining#anyOf
-            // NOTE: If is expected that the "oneOf" candidate should hava an if-else-then or
+            // NOTE: If is expected that the "anyOf" candidate should hava an if-else-then or
             // if-then nodes
             // inside, otherwise the condition will not be evaluated and only the first occurrence
             // appearing in the list
