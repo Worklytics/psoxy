7169bbc34f9872b2959b1b8673049558be89a10c
andres <andres@worklytics.co>
2025-08-01 01:11:17 +0200

    Drop hyphen

--- java/core/src/test/java/co/worklytics/psoxy/rules/chatgpt/ChatGPTComplianceTests.java
+++ java/core/src/test/java/co/worklytics/psoxy/rules/chatgpt/ChatGPTComplianceTests.java
@@ -12,9 +12,9 @@ public class ChatGPTComplianceTests extends JavaRulesTestBaseCase {
     final RESTRules rulesUnderTest = PrebuiltSanitizerRules.COMPLIANCE;
 
     final RulesTestSpec rulesTestSpec = RulesTestSpec.builder()
-        .defaultScopeId("chat-gpt")
-        .sourceKind("chat-gpt")
-        .rulesFile("compliance/chat-gpt-compliance")
+        .defaultScopeId("chatgpt")
+        .sourceKind("chatgpt")
+        .rulesFile("compliance/chatgpt-compliance")
         .exampleApiResponsesDirectoryPath("compliance/example-api-responses/original/")
         .exampleSanitizedApiResponsesPath("compliance/example-api-responses/sanitized/")
         .checkUncompressedSSMLength(false)
