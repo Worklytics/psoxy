9171c37615d3a0062f97c1ebe625ca045677a435
Erik Schultink <eschultink@users.noreply.github.com>
2025-09-22 14:45:32 -0700

    why ide crazy ...

--- infra/examples-dev/gcp/google-workspace-variables.tf
+++ infra/examples-dev/gcp/google-workspace-variables.tf
@@ -39,7 +39,7 @@ variable "google_workspace_key_rotation_days" {
 
   validation {
     condition     = var.google_workspace_key_rotation_days > 0
-    error_message = "google_workspace_key_rotation_days_rotation_days must be greater than 0"
+    error_message = "google_workspace_key_rotation_days must be greater than 0"
   }
 }
 
