cc25f856b99b72a156fd6fed48c2187891c70d59
Erik Schultink <eschultink@users.noreply.github.com>
2025-10-06 15:48:46 -0700

    update release refs to v0.5.10

--- infra/examples-dev/gcp/google-workspace.tf
+++ infra/examples-dev/gcp/google-workspace.tf
@@ -8,7 +8,7 @@ provider "google" {
 
 module "worklytics_connectors_google_workspace" {
   source = "../../modules/worklytics-connectors-google-workspace"
-  # source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-google-workspace?ref=rc-v0.5.10"
+  # source = "git::https://github.com/worklytics/psoxy//infra/modules/worklytics-connectors-google-workspace?ref=v0.5.10"
 
   providers = {
     google = google.google_workspace
@@ -17,7 +17,7 @@ module "worklytics_connectors_google_workspace" {
   environment_id                 = var.environment_name
   enabled_connectors             = var.enabled_connectors
   gcp_project_id                 = var.google_workspace_gcp_project_id
-  tf_gcp_principal_email         = var.google_workspace_terraform_sa_account_email
+  tf_gcp_principal_email         = var.google_workspace_terraform_principal_email
   google_workspace_example_user  = var.google_workspace_example_user
   google_workspace_example_admin = var.google_workspace_example_admin
   provision_gcp_sa_keys          = var.google_workspace_provision_keys
