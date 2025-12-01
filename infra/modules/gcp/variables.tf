4147bbb1f78372a7e8edd3d71cdb975e320ddcbe
Jose Lorenzo <jose@worklytics.co>
2025-12-01 19:44:25 +0100

    Use try to check

--- infra/modules/gcp/variables.tf
+++ infra/modules/gcp/variables.tf
@@ -117,17 +117,14 @@ variable "vpc_config" {
     # no config at all
     var.vpc_config == null ||
     # serverless connector referenced
-    (var.vpc_config != null &&
-    var.vpc_config.serverless_connector != null) ||
+    try(var.vpc_config.serverless_connector, null) != null) &&
     # network and subnetwork defined
     (
-    var.vpc_config != null &&
-    var.vpc_config.serverless_connector == null &&
-    var.vpc_config.network != null &&
-    var.vpc_config.subnet != null &&
+    (try(var.vpc_config.serverless_connector, null) == null) &&
+    (try(var.vpc_config.network, null) != null) &&
+    (try(var.vpc_config.subnet, null) != null) &&
     ((!startswith(var.vpc_config.network, "projects/") && !startswith(var.vpc_config.subnet, "projects/")) ||
     (startswith(var.vpc_config.network, "projects/") && startswith(var.vpc_config.subnet, "projects/")))
-    )
     )
     error_message = "Invalid vpc_config: Must provide either serverless_connector, or valid network/subnet as described in the documentation."
   }
