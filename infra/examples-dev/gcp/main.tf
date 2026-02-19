2315bcc946be78d68666e78589b1ad398cf12d61
andres <andres@worklytics.co>
2026-02-19 12:31:16 +0100

    Examples support

--- infra/examples-dev/gcp/main.tf
+++ infra/examples-dev/gcp/main.tf
@@ -46,6 +46,8 @@ module "worklytics_connectors" {
   github_copilot_installation_id           = var.github_copilot_installation_id
   github_organization                      = var.github_organization
   github_example_repository                = var.github_example_repository
+  gitlab_host                              = var.gitlab_host
+  gitlab_example_group_id                  = var.gitlab_example_group_id
   salesforce_example_account_id            = var.salesforce_example_account_id
   todos_as_local_files                     = var.todos_as_local_files
   todo_step                                = 1
