d5da79770e1f556253bff7f890b6902d8a73908e
Erik Schultink <eschultink@users.noreply.github.com>
2026-04-29 14:35:34 -0700

    mark deprecated; migrate validations

--- infra/modules/worklytics-connector-specs/variables.tf
+++ infra/modules/worklytics-connector-specs/variables.tf
@@ -11,20 +11,20 @@ variable "base_dir" {
 
 variable "chat_gpt_enterprise_example_workspace_id" {
   type        = string
-  description = "Workspace id to use for example calls"
+  description = "[DEPRECATED - use connector_settings map instead] Workspace id to use for example calls"
   default     = null
 }
 
 variable "confluence_example_cloud_id" {
   type        = string
   default     = null
-  description = "(Only required if using Confluence Cloud connector) Example of cloud id of the Confluence Cloud to connect to (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)."
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Confluence Cloud connector) Example of cloud id of the Confluence Cloud to connect to (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)."
 }
 
 variable "confluence_example_group_id" {
   type        = string
   default     = null
-  description = "(Only required if using Confluence Cloud connector) Example of group id of the Confluence Cloud to connect to (ex: 35e417ad-bcb1-45fe-9be0-959239a84327)."
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Confluence Cloud connector) Example of group id of the Confluence Cloud to connect to (ex: 35e417ad-bcb1-45fe-9be0-959239a84327)."
 }
 
 variable "google_workspace_example_user" {
@@ -101,39 +101,39 @@ variable "msft_teams_example_online_meeting_join_url" {
 
 variable "salesforce_domain" {
   type        = string
-  description = "Domain of the Salesforce to connect to (only required if using Salesforce connector). To find your My Domain URL, from Setup, in the Quick Find box, enter My Domain, and then select My Domain"
+  description = "[DEPRECATED - use connector_settings map instead] Domain of the Salesforce to connect to (only required if using Salesforce connector). To find your My Domain URL, from Setup, in the Quick Find box, enter My Domain, and then select My Domain"
   default     = ""
 }
 
 variable "jira_server_url" {
   type        = string
   default     = null
-  description = "(Only required if using Jira Server connector) URL of the Jira server (ex: myjiraserver.mycompany.com)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Jira Server connector) URL of the Jira server (ex: myjiraserver.mycompany.com)"
 }
 
 variable "jira_cloud_id" {
   type        = string
   default     = null
-  description = "(Only required if using Jira Cloud connector) Example of cloud id of the Jira Cloud to connect to (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)."
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Jira Cloud connector) Example of cloud id of the Jira Cloud to connect to (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)."
 }
 
 #DEPRECATED
 variable "example_jira_issue_id" {
   type        = string
   default     = null
-  description = "Deprecated; use `jira_example_issued_id`. (Only required if using Jira Server/Cloud connector) Id of an issue for only to be used as part of example calls for Jira (ex: ETV-12)"
+  description = "[DEPRECATED - use connector_settings map instead] Deprecated; use `jira_example_issued_id`. (Only required if using Jira Server/Cloud connector) Id of an issue for only to be used as part of example calls for Jira (ex: ETV-12)"
 }
 
 variable "jira_example_issue_id" {
   type        = string
   default     = null
-  description = "If using Jira Server/Cloud connector, provide id of an issue for only to be used as part of example calls for Jira (ex: ETV-12)"
+  description = "[DEPRECATED - use connector_settings map instead] If using Jira Server/Cloud connector, provide id of an issue for only to be used as part of example calls for Jira (ex: ETV-12)"
 }
 
 variable "atlassian_organization_id" {
   type        = string
   default     = null
-  description = "(Only required if using Atlassian Organization connector) ID of the Atlassian Organization (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Atlassian Organization connector) ID of the Atlassian Organization (ex: 1324a887-45db-1bf4-1e99-ef0ff456d421)"
 }
 
 # DEPRECATED
@@ -146,73 +146,73 @@ variable "github_api_host" {
 variable "github_enterprise_server_host" {
   type        = string
   default     = "" # not null, as this is required if using github_enterprise_server. Validation done on the caller
-  description = "(Only required if using Github Enterprise Server connector) Host of the Github instance (ex: github.mycompany.com)."
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github Enterprise Server connector) Host of the Github instance (ex: github.mycompany.com)."
 }
 
 variable "github_enterprise_server_version" {
   type        = string
   default     = "v3"
-  description = "(Only required if using Github Enterprise Server connector) Version of the server to use (ex: v3). By default, v3"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github Enterprise Server connector) Version of the server to use (ex: v3). By default, v3"
 }
 
 variable "github_installation_id" {
   type        = string
   default     = null
-  description = "(Only required if using Github connector) InstallationId of the application in your org for authentication with the proxy instance (ex: 123456)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github connector) InstallationId of the application in your org for authentication with the proxy instance (ex: 123456)"
 }
 
 variable "github_copilot_installation_id" {
   type        = string
   default     = null
-  description = "(Only required if using Github Copilot connector) InstallationId of the application in your org for authentication with the proxy instance (ex: 123456)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github Copilot connector) InstallationId of the application in your org for authentication with the proxy instance (ex: 123456)"
 }
 
 variable "github_organization" {
   type        = string
   default     = null
-  description = "(Only required if using Github connector) Name of the organization to be used as part of example calls for Github (ex: Worklytics). NOTE: If using Enterprise Server, this can be a list of organizations split by commas (ex: Worklytics,Worklytics2)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github connector) Name of the organization to be used as part of example calls for Github (ex: Worklytics). NOTE: If using Enterprise Server, this can be a list of organizations split by commas (ex: Worklytics,Worklytics2)"
 }
 
 variable "github_example_repository" {
   type        = string
   default     = null
-  description = "(Only required if using Github connector) Name for the repository to be used as part of example calls for Github (ex: psoxy)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Github connector) Name for the repository to be used as part of example calls for Github (ex: psoxy)"
 }
 
 variable "gitlab_url" {
   type        = string
   default     = "https://gitlab.com"
-  description = "(Only required if using GitLab Managed connector) URL of the GitLab instance (ex: https://gitlab.mycompany.com, https://mycompany.com/gitlab, https://my-instance-gitlab.com, etc)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using GitLab Managed connector) URL of the GitLab instance (ex: https://gitlab.mycompany.com, https://mycompany.com/gitlab, https://my-instance-gitlab.com, etc)"
 }
 
 variable "gitlab_example_group_id" {
   type        = string
   default     = null
-  description = "(Only required if using GitLab connector) Example group ID for test API calls (ex: 12345)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using GitLab connector) Example group ID for test API calls (ex: 12345)"
 }
 
 variable "gitlab_example_project_id" {
   type        = string
   default     = null
-  description = "(Only required if using GitLab connector) Example project ID for test API calls (ex: 12345)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using GitLab connector) Example project ID for test API calls (ex: 12345)"
 }
 
 variable "glean_instance_subdomain" {
   type        = string
   default     = null
-  description = "(Only required if using Glean connector) Subdomain of your Glean instance (ex: if your Glean URL is 'acme-be.glean.com', the instance subdomain is 'acme-be')"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Glean connector) Subdomain of your Glean instance (ex: if your Glean URL is 'acme-be.glean.com', the instance subdomain is 'acme-be')"
 }
 
 variable "gong_instance_subdomain" {
   type        = string
   default     = null
-  description = "(Only required if using Gong connector) Subdomain of your Gong instance (ex: if your Gong URL is 'acme.gong.io', the instance subdomain is 'acme')"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Gong connector) Subdomain of your Gong instance (ex: if your Gong URL is 'acme.gong.io', the instance subdomain is 'acme')"
 }
 
 variable "salesforce_example_account_id" {
   type        = string
   default     = null
-  description = "(Only required if using Salesforce connector) Id of the account id for using as an example calls for Salesforce (ex: 0015Y00002c7g95QAA)"
+  description = "[DEPRECATED - use connector_settings map instead] (Only required if using Salesforce connector) Id of the account id for using as an example calls for Salesforce (ex: 0015Y00002c7g95QAA)"
 }
 
 variable "example_api_calls_sample_date" {
